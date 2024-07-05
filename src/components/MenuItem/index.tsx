c3ece4112656715c081d484723439568c0189960
Matheus Quevedo <matheusq10@gmail.com>
2024-07-05 00:09:07 -0300

    Adição: foi adicionada a função de verificar o path ativo com o useMatch

--- src/components/MenuItem/index.tsx
+++ src/components/MenuItem/index.tsx
@@ -1,17 +1,19 @@
-import { Link } from "react-router-dom";
-
-interface MenuItemProps {
+import { Link, LinkProps, useMatch } from "react-router-dom";
+interface MenuItemProps extends LinkProps {
     icon: string;
     text: string;
     altText: string;
-    to: string;
 }
 
-export function MenuItem({ text, icon, altText, to }: MenuItemProps) {
+export function MenuItem({ text, icon, altText, to, ...props }: MenuItemProps) {
+    const isActive = useMatch(to.toString())
+    const activeTextColor = "text-menucyan font-bold"
+    const textStyle = isActive ? activeTextColor : ""
+
     return (
-        <Link to={to} className="flex items-center gap-2 focus:bg-black">
-            <img src={icon} alt={altText} className="h-auto w-9 " />
-            <p className="pt-3">{text}</p>
+        <Link {...props} to={to} className="flex items-center gap-2">
+            <img src={icon} alt={altText} className="h-auto w-9" />
+            <p className={textStyle + " pt-3"}>{text}</p>
         </Link>
     );
 }
