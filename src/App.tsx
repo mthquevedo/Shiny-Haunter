9e3ba697b1148f0832228550d50af187f8933a77
Matheus Quevedo <matheusq10@gmail.com>
2024-07-07 18:52:29 -0300

    Criação: novas rotas foram criadas e a rota padrão para erro de url foi redefinida

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import { MainLayout } from "./layouts/MainLayout"
 import { HomePage } from "./pages/HomePage"
+import { Pokedex } from "./pages/Pokedex"
+import { Shinys } from "./pages/Shinys"
+import { Wishlist } from "./pages/Wishlist"
 
 function App() {
 
@@ -8,12 +11,12 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
-          <Route path="/" element={<HomePage />} />
-
-          <Route path="pokemon" >
-            <Route path="list" element={<div>Conteudo</div>} />
-            <Route path="*" element={<Navigate to="/pokemon/list" />} />
+          <Route path="/" element={<HomePage />}>
+            <Route path="*" element={<Navigate to="/" />} />
           </Route>
+          <Route path="pokedex" element={<Pokedex />} />
+          <Route path="listadedesejos" element={<Wishlist />} />
+          <Route path="shinys" element={<Shinys />} />
         </Route>
       </Routes>
     </BrowserRouter>
