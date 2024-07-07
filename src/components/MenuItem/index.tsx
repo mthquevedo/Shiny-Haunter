e067ada33730814875ad4684d779ea307bc3d14c
Matheus Quevedo <matheusq10@gmail.com>
2024-07-07 18:53:46 -0300

    Ajuste: os espaçamentos foram ajustados + ajustes feitos no item do menu ativo

--- src/components/MenuItem/index.tsx
+++ src/components/MenuItem/index.tsx
@@ -5,17 +5,21 @@ interface MenuItemProps extends LinkProps {
     text: string;
     altText: string;
 }
+
 const ACTIVE_TEXT_COLOR = "text-menucyan font-bold";
+const ACTIVE_BG = "transition ease-in-out delay-50 bg-backcolor/10 pl-5 pr-0 w-56 border-l-4 border-menucyan rounded-tr-lg rounded-br-lg";
 
 export function MenuItem({ text, icon, altText, to, ...props }: MenuItemProps) {
     const isActive = useMatch(to.toString())
 
     return (
-        <Link {...props} to={to} className="flex items-center gap-2">
-            <img src={icon} alt={altText} className="h-auto w-9" />
-            <p className={cn("pt-3", {[ACTIVE_TEXT_COLOR]: isActive})}>
-                {text}
-            </p>
-        </Link>
+        <div className={cn("py-2 pl-6 pr-6", { [ACTIVE_BG]: isActive })}>
+            <Link {...props} to={to} className="flex items-center gap-2">
+                <img src={icon} alt={altText} className="h-auto w-9" />
+                <p className={cn("pt-1", { [ACTIVE_TEXT_COLOR]: isActive })}>
+                    {text}
+                </p>
+            </Link>
+        </div>
     );
 }
