4544e8f723b6ec01e2b5ffb1e7f81f7d641cf747
Matheus Quevedo <matheusq10@gmail.com>
2024-07-07 23:08:50 -0300

    Criação: o botão para mudar o tema foi criado

---
+++ src/components/ThemeToggle/index.tsx
@@ -0,0 +1,9 @@
+export function ThemeToggle() {
+    return (
+        <div className="w-16 h-6 flex items-center cursor-pointer rounded-full p-1 bg-white">
+            <div className="absolute bg-buttoncyanhigh w-4 h-4 rounded-full shadow-md">
+
+            </div>
+        </div>
+    )
+}
