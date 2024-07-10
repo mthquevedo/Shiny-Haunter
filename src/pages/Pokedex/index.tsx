2a446d9f9f6eb6150d0fdf746f81b081232b5b25
Matheus Quevedo <matheusq10@gmail.com>
2024-07-10 17:17:39 -0300

    Criação: os elementos de header, searchform e footer foram adicionados a página

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -1,3 +1,18 @@
+import { FooterPage } from "../../components/FooterPage";
+import { HeaderPage } from "../../components/HeaderPage";
+import { SearchForm } from "../../components/SearchForm";
+
 export function Pokedex() {
-    return ("")
+    return (
+        <section className="bg-backcolor w-85v h-100v pt-7 pb-9 pl-10 pr-10 flex flex-col justify-between">
+            <HeaderPage />
+
+            <SearchForm
+                formTitle="Pokédex"
+                formDescription="Procure por um pokémon específico ou explore a pokedex de cada região"
+            />
+
+            <FooterPage />
+        </section>
+    );
 }
