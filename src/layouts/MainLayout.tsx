a0d075e65a79ce66c16566721f80f8b13b3307f8
Matheus Quevedo <matheusq10@gmail.com>
2024-07-10 14:30:45 -0300

    Ajuste: o logotipo foi componentizado

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { MenuItem } from "../components/Menu/MenuItem";
 import { ThemeToggle } from "../components/Menu/ThemeToggle";
+import { Logo } from "../components/Menu/Logo";
 
 export const MainLayout = () => {
 
@@ -8,13 +9,7 @@ export const MainLayout = () => {
         <section className="w-screen min-h-screen flex font-poppins">
             <aside className="bg-primary w-15v min-w-44 h-100v pt-7 pb-8 border-r border-indigo-400 flex flex-col justify-between">
                 <div>
-                    <div className="flex gap-2 items-center mb-2 pl-6 pr-6">
-                        <img className="h-16 w-16 rounded" src="src/assets/logo.jpg" alt="logotipo" />
-                        <div className="text-menucyan font-semibold text-3xl">
-                            <p>Shiny</p>
-                            <p>Haunter</p>
-                        </div>
-                    </div>
+                    <Logo />
 
                     <nav className="mt-11 flex flex-col gap-7 text-gray-50">
                         <MenuItem icon={"src/assets/pokeball.png"} altText={"Ícone do Início"} text={"Início"} to={"/"} />
