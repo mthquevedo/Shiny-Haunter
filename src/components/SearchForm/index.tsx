b9ed99009cf38f1dcbcc203a9518ae6d6cfbf764
Matheus Quevedo <matheusq10@gmail.com>
2024-07-12 21:30:23 -0300

    Adição: uma sombra e borda foram adicionados ao componente de pesquisa

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -14,7 +14,7 @@ export function SearchForm({ formTitle, formDescription }: SearchForm) {
                 <p className="text-darkgray">{formDescription}</p>
             </div>
 
-            <div className="flex items-center justify-between gap-2 bg-white rounded-full w-fit py-1.5 px-5">
+            <div className="flex items-center justify-between gap-2 bg-white rounded-full w-fit py-1.5 px-5 border shadow">
                 <div className="flex items-center gap-2">
                     <IoSearch className="fill-primary w-5 h-5" />
                     <input className="text-sm w-20v h-6 placeholder:font-light placeholder:italic focus:outline-none" spellCheck="false" type="text" placeholder="Busque pelo nome do pokémon" id="search" />
