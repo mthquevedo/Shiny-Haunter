cfd5c7f694b1aa44265e9389eb263559ec5eb4f8
Matheus Quevedo <matheusq10@gmail.com>
2024-09-15 13:45:39 -0300

    Criação: a interface de props foi criada e aplicada no componente

--- src/components/PokeCard/index.tsx
+++ src/components/PokeCard/index.tsx
@@ -1,26 +1,35 @@
+import { upperFirstLetter } from "../../utils/pokemon.utils";
 import { TypeFlag } from "../TypeFlag";
 
-export function PokeCard() {
+interface PokeCardProps {
+    id: number;
+    region: string;
+    name: string;
+    thumbnail: string;
+    firstType: string;
+    secondType: string;
+}
+
+export function PokeCard({ id, name, region, thumbnail, firstType, secondType }: PokeCardProps) {
+
     return (
         <article
             className="flex flex-col bg-white w-15v mt-3 py-3 px-4 rounded-md shadow-md">
             <section className="flex flex-col justify-between bg-electricback w-full p-2">
                 <div>
-                    <p>#999</p>
-                    <p>Paldea</p>
+                    <p>#{id}</p>
+                    <p>{region}</p>
                 </div>
 
-                <img src="./src/assets/1008.png" alt="" className="" />
+                <img src={thumbnail} alt="" className="" />
 
-                <p>Miraidon</p>
-
+                <p>{name}</p>
             </section>
 
             <footer className="flex justify-between items-center mt-2">
                 <div className="flex gap-2">
-                    <TypeFlag type="Electric" flagColor="bg-electric" />
-                    {/* <TypeFlag icon={getPokemonTypeSVG("electric")} type="type" flagColor="bg-electric" />
-                    <TypeFlag icon="./src/assets/typesIcons/dragon.svg" type="Dragon" flagColor="bg-dragon" /> */}
+                    <TypeFlag type={upperFirstLetter(firstType)} icon={firstType} flagColor={"bg-" + firstType} />
+                    <TypeFlag type={upperFirstLetter(secondType)} icon={secondType} flagColor={"bg-" + secondType} />
                 </div>
 
                 <button className="text-sm">
