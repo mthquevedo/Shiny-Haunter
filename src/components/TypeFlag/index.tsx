c0ece6d391d508b0c6af8d00abbdb25cd23ace4d
Matheus Quevedo <matheusq10@gmail.com>
2024-09-15 17:06:51 -0300

    Ajuste: novos ajustes visuais

--- src/components/TypeFlag/index.tsx
+++ src/components/TypeFlag/index.tsx
@@ -9,12 +9,12 @@ interface TypeProps {
 
 export function TypeFlag({ type, flagColor, icon }: TypeProps) {
     return (
-        <div className={twMerge("flex items-center gap-1 px-2 py-1 rounded-full", flagColor)}>
-            <span className="w-7 h-7 bg-white rounded-full flex justify-center items-center">
-                <img src={getPokemonTypeSVG(icon)} alt="" className="w-5 h-5" />
+        <div className={twMerge("flex items-center w-[5.313rem] gap-1 px-1.5 py-1 rounded-full", flagColor)}>
+            <span className="w-5 h-5 bg-white rounded-full flex justify-center items-center">
+                <img src={getPokemonTypeSVG(icon)} alt="" className="w-4 h-4" />
 
             </span>
-            <p className="text-sm font-medium">{type}</p>
+            <p className="text-xs font-medium">{type}</p>
         </div>
     );
 }
