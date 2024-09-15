606355c17040c7543f425b736d4cdeddc1763b69
Matheus Quevedo <matheusq10@gmail.com>
2024-09-15 13:49:23 -0300

    Ajuste: as configurações de ícone foram ajustadas

--- src/components/TypeFlag/index.tsx
+++ src/components/TypeFlag/index.tsx
@@ -1,17 +1,18 @@
 import { twMerge } from "tailwind-merge";
-import { pokemonTypes } from "../../utils/pokemon.utils";
+import { getPokemonTypeSVG } from "../../utils/pokemon.utils";
 
 interface TypeProps {
-    type: keyof typeof pokemonTypes;
+    type: string;
+    icon: string;
     flagColor: string;
 }
 
-export function TypeFlag({ type, flagColor }: TypeProps) {
+export function TypeFlag({ type, flagColor, icon }: TypeProps) {
     return (
         <div className={twMerge("flex items-center gap-1 px-2 py-1 rounded-full", flagColor)}>
             <span className="w-7 h-7 bg-white rounded-full flex justify-center items-center">
-                <pokemonTypes[type]/>
-                {pokemonTypes?.[type] && <pokemonTypes.[type]/>}
+                <img src={getPokemonTypeSVG(icon)} alt="" className="w-5 h-5" />
+
             </span>
             <p className="text-sm font-medium">{type}</p>
         </div>
