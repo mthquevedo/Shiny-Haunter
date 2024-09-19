934f7460b91246816a7cb1227a37eefaee181be0
Matheus Quevedo <matheusq10@gmail.com>
2024-09-18 21:23:31 -0300

    Criação: a lógica de cores por tipo foi criada

--- src/utils/pokemon.utils.ts
+++ src/utils/pokemon.utils.ts
@@ -1,3 +1,5 @@
+import { ClassValue } from "clsx";
+
 const SVG_PATH = "./src/assets/typesIcons/";
 
 export function getPokemonTypeSVG(type: string) {
@@ -6,4 +8,14 @@ export function getPokemonTypeSVG(type: string) {
 
 export function upperFirstLetter(type: string) {
     return type.charAt(0).toLocaleUpperCase() + type.slice(1);
+}
+
+export const typeColor: Record<string, ClassValue> = {
+    dragon: "bg-dragon",
+    electric: "bg-electric",
+}
+
+export const bannerTypeColor: Record<string, ClassValue> = {
+    dragon: "bg-dragonback",
+    electric: "bg-electricback",
 }
