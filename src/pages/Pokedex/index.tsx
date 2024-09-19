1a6d709df8f12b3589b14e948da5ec4c973a2f05
Matheus Quevedo <matheusq10@gmail.com>
2024-09-18 21:24:02 -0300

    Comentários: novos trechos foram comentados

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -1,21 +1,27 @@
-// import { NamedAPIResourceList, PokemonClient } from "pokenode-ts";
 // import { useEffect, useState } from "react";
 import { FooterPage } from "../../components/FooterPage";
 import { HeaderPage } from "../../components/HeaderPage";
-import { PokedexFrame } from "../../components/PokedexFrame";
+import { PokeCard } from "../../components/PokeCard";
 import { SearchForm } from "../../components/SearchForm";
+// import { NamedAPIResourceList, Pokemon, PokemonClient } from 'pokenode-ts';
 
 export function Pokedex() {
-    // const [pokemon, setPokemon] = useState<NamedAPIResourceList>()
+    // const [pokemon, setPokemons] = useState<NamedAPIResourceList>()
+    // const [pokemon, setPokemons] = useState<Pokemon>()
 
     // useEffect(() => {
-    //     const api = new PokemonClient();
     //     (async () => {
-    //         return await api.listPokemons(0, 10).then((data) => {
-    //             setPokemon(data)
-    //         })
+    //         const api = new PokemonClient();
+
+    //         await api
+    //             .listPokemons(0, 10)
+    //             .then((data) => console.log())
+    //         // .then((data) => setPokemons(data))
     //     })()
     // }, [])
+
+    // console.log(pokemon);
+
 
     return (
         <section className="bg-backcolor w-85v h-100v pt-7 pb-9 pl-10 pr-10 flex flex-col justify-between">
@@ -26,13 +32,42 @@ export function Pokedex() {
                 formDescription="Procure por um pokémon específico ou explore a pokedex de cada região"
             />
 
+            {/* {pokemon && <img src={pokemon.results ?? ""} />} */}
             {/* {pokemon && <img src={pokemon.sprites.other?.["official-artwork"].front_default ?? ""} />} */}
-            {/* 
-            <pre>
-                {pokemon && JSON.stringify(pokemon, null, 2)}
-            </pre> */}
 
-            <PokedexFrame />
+            {/* 
+           <pre>
+               {pokemons && JSON.stringify(pokemons, null, 2)}
+           </pre> */}
+
+            <section className="flex flex-col gap-1 w-full h-75v">
+                <div>
+                    <p className="bg-slate-500 text-white p-1">regiões...</p>
+                </div>
+                <div className="flex flex-wrap gap-4 pl-1">
+                    {/* {pokemons.map(pokemon => {
+                        return (
+                            <PokeCard
+                                id={999}
+                                name="Miraidon"
+                                region="Paldea"
+                                thumbnail="./src/assets/1008.png"
+                                firstType="electric"
+                                secondType="dragon"
+                            />
+                        );
+                    })} */}
+
+                    <PokeCard
+                        id={999}
+                        name="Miraidon"
+                        region="Paldea"
+                        thumbnail="./src/assets/1008.png"
+                        firstType="electric"
+                        secondType="dragon"
+                    />
+                </div>
+            </section>
 
             <FooterPage />
         </section>
