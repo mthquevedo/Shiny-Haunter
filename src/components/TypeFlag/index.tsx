5488c117943a9c06a6b910b4c881643b70594613
Matheus Quevedo <matheusq10@gmail.com>
2024-09-18 21:24:42 -0300

    Ajuste: as props foram atualizadas para receber apenas um tipo

--- src/components/TypeFlag/index.tsx
+++ src/components/TypeFlag/index.tsx
@@ -1,20 +1,18 @@
-import { twMerge } from "tailwind-merge";
-import { getPokemonTypeSVG } from "../../utils/pokemon.utils";
+import { cn } from "../../lib/cn";
+import { getPokemonTypeSVG, typeColor, upperFirstLetter } from "../../utils/pokemon.utils";
 
 interface TypeProps {
     type: string;
-    icon: string;
-    flagColor: string;
 }
 
-export function TypeFlag({ type, flagColor, icon }: TypeProps) {
+export function TypeFlag({ type }: TypeProps) {
     return (
-        <div className={twMerge("flex items-center w-[5.313rem] gap-1 px-1.5 py-1 rounded-full", flagColor)}>
+        <div className={cn("flex items-center w-[5.313rem] gap-1 px-1.5 py-1 rounded-full", typeColor[type])}>
             <span className="w-5 h-5 bg-white rounded-full flex justify-center items-center">
-                <img src={getPokemonTypeSVG(icon)} alt="" className="w-4 h-4" />
+                <img src={getPokemonTypeSVG(type)} alt="" className="w-4 h-4" />
 
             </span>
-            <p className="text-xs font-medium">{type}</p>
+            <p className="text-xs font-medium">{upperFirstLetter(type)}</p>
         </div>
     );
 }
