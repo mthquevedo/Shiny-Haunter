4a258b416c96690190d5eb94ce4f5d01ff1ea37c
Matheus Quevedo <matheusq10@gmail.com>
2024-10-14 20:25:27 -0300

    Ajuste: comentários e funções antigas foram removidas + foi criada a conexão com os poke services

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -1,27 +1,12 @@
-// import { useEffect, useState } from "react";
 import { FooterPage } from "../../components/FooterPage";
 import { HeaderPage } from "../../components/HeaderPage";
 import { PokeCard } from "../../components/PokeCard";
 import { SearchForm } from "../../components/SearchForm";
-// import { NamedAPIResourceList, Pokemon, PokemonClient } from 'pokenode-ts';
+import { upperFirstLetter } from "../../utils/pokemon.utils";
+import { usePokedex } from "./pokedex.hook";
 
 export function Pokedex() {
-    // const [pokemon, setPokemons] = useState<NamedAPIResourceList>()
-    // const [pokemon, setPokemons] = useState<Pokemon>()
-
-    // useEffect(() => {
-    //     (async () => {
-    //         const api = new PokemonClient();
-
-    //         await api
-    //             .listPokemons(0, 10)
-    //             .then((data) => console.log())
-    //         // .then((data) => setPokemons(data))
-    //     })()
-    // }, [])
-
-    // console.log(pokemon);
-
+    const { pokemons, handleNextPage, handlePreviousPage, isLoading, next, previous } = usePokedex();
 
     return (
         <section className="bg-backcolor w-85v h-100v pt-7 pb-9 pl-10 pr-10 flex flex-col justify-between">
@@ -32,42 +17,39 @@ export function Pokedex() {
                 formDescription="Procure por um pokémon específico ou explore a pokedex de cada região"
             />
 
-            {/* {pokemon && <img src={pokemon.results ?? ""} />} */}
-            {/* {pokemon && <img src={pokemon.sprites.other?.["official-artwork"].front_default ?? ""} />} */}
-
-            {/* 
-           <pre>
-               {pokemons && JSON.stringify(pokemons, null, 2)}
-           </pre> */}
-
             <section className="flex flex-col gap-1 w-full h-75v">
                 <div>
-                    <p className="bg-slate-500 text-white p-1">regiões...</p>
+                    <p className="bg-slate-500 text-white p-1 my-2">regiões...</p>
                 </div>
                 <div className="flex flex-wrap gap-4 pl-1">
-                    {/* {pokemons.map(pokemon => {
+                    {pokemons && pokemons.map(pokemon => {
                         return (
                             <PokeCard
-                                id={999}
-                                name="Miraidon"
+                                id={pokemon.id}
+                                name={upperFirstLetter(pokemon.name)}
                                 region="Paldea"
-                                thumbnail="./src/assets/1008.png"
-                                firstType="electric"
-                                secondType="dragon"
+                                thumbnail={pokemon.sprites.other?.["official-artwork"].front_shiny || ""}
+                                firstType={pokemon.types[0].type.name}
+                                secondType={pokemon.types[1] ? pokemon.types[1].type.name : ""}
                             />
-                        );
-                    })} */}
 
-                    <PokeCard
+                        );
+                    })}
+
+
+                    {/* <PokeCard
                         id={999}
                         name="Miraidon"
                         region="Paldea"
                         thumbnail="./src/assets/1008.png"
                         firstType="electric"
                         secondType="dragon"
-                    />
+                    /> */}
                 </div>
             </section>
+
+            <button onClick={handlePreviousPage} disabled={!previous}>Anterior</button>
+            <button onClick={handleNextPage} disabled={!next}>Próximo</button>
 
             <FooterPage />
         </section>
