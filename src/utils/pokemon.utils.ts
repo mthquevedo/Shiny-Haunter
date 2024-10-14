6c68748786c5fb640ee64ee1db0192c5bf6f53da
Matheus Quevedo <matheusq10@gmail.com>
2024-10-14 20:24:16 -0300

    Adição: novos tipos foram adicionados as funções que controlam o visual da tipagem

--- src/utils/pokemon.utils.ts
+++ src/utils/pokemon.utils.ts
@@ -11,11 +11,44 @@ export function upperFirstLetter(type: string) {
 }
 
 export const typeColor: Record<string, ClassValue> = {
+    bug: "bg-bug",
+    dark: "bg-dark",
     dragon: "bg-dragon",
     electric: "bg-electric",
-}
+    fairy: "bg-fairy",
+    fighting: "bg-fighting",
+    fire: "bg-fire",
+    flying: "bg-flying",
+    ghost: "bg-ghost",
+    grass: "bg-grass",
+    ground: "bg-ground",
+    ice: "bg-ice",
+    normal: "bg-normal",
+    poison: "bg-poison",
+    psychic: "bg-psychic",
+    rock: "bg-rock",
+    steel: "bg-steel",
+    water: "bg-water",
+};
+
 
 export const bannerTypeColor: Record<string, ClassValue> = {
+    bug: "bg-bugback",
+    dark: "bg-darkback",
     dragon: "bg-dragonback",
     electric: "bg-electricback",
-}
+    fairy: "bg-fairyback",
+    fighting: "bg-fightingback",
+    fire: "bg-fireback",
+    flying: "bg-flyingback",
+    ghost: "bg-ghostback",
+    grass: "bg-grassback",
+    ground: "bg-groundback",
+    ice: "bg-iceback",
+    normal: "bg-normalback",
+    poison: "bg-poisonback",
+    psychic: "bg-psychicback",
+    rock: "bg-rockback",
+    steel: "bg-steelback",
+    water: "bg-waterback",
+};
