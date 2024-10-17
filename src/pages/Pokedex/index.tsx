98cbef631a539058673ac3389ad5b9a60d600346
Matheus Quevedo <matheusq10@gmail.com>
2024-10-16 23:05:04 -0300

    Ajuste: atualizações visuais

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -9,7 +9,7 @@ export function Pokedex() {
     const { pokemons, handleNextPage, handlePreviousPage, isLoading, next, previous } = usePokedex();
 
     return (
-        <section className="bg-backcolor w-85v h-100v pt-7 pb-9 pl-10 pr-10 flex flex-col justify-between">
+        <section className="bg-gradient-to-tl from-backcolor to-white w-95v h-100v pt-11 pb-9 px-10 flex flex-1 flex-col justify-between">
             <HeaderPage />
 
             <SearchForm
@@ -17,18 +17,18 @@ export function Pokedex() {
                 formDescription="Procure por um pokémon específico ou explore a pokedex de cada região"
             />
 
-            <section className="flex flex-col gap-1 w-full h-75v">
+            <section className="flex flex-col  gap-1 w-full h-75v">
                 <div>
-                    <p className="bg-slate-500 text-white p-1 my-2">regiões...</p>
+                    <p className="bg-slate-500 text-white rounded-md p-1 px-2 my-2">regiões...</p>
                 </div>
-                <div className="flex flex-wrap gap-4 pl-1">
+                <div className="flex item-center justify-center flex-wrap gap-6 pl-1">
                     {pokemons && pokemons.map(pokemon => {
                         return (
                             <PokeCard
                                 id={pokemon.id}
                                 name={upperFirstLetter(pokemon.name)}
-                                region="Paldea"
                                 thumbnail={pokemon.sprites.other?.["official-artwork"].front_shiny || ""}
+                                // thumbnail={pokemon.sprites.front_shiny || ""}
                                 firstType={pokemon.types[0].type.name}
                                 secondType={pokemon.types[1] ? pokemon.types[1].type.name : ""}
                             />
@@ -48,9 +48,10 @@ export function Pokedex() {
                 </div>
             </section>
 
-            <button onClick={handlePreviousPage} disabled={!previous}>Anterior</button>
-            <button onClick={handleNextPage} disabled={!next}>Próximo</button>
-
+            <div className="flex gap-2 items-center justify-center">
+                <button onClick={handlePreviousPage} disabled={!previous}>Anterior</button>
+                <button onClick={handleNextPage} disabled={!next}>Próximo</button>
+            </div>
             <FooterPage />
         </section>
     );
