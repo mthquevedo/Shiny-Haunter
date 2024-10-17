28dda771c2921a824e333dd1592747ae85b17a48
Matheus Quevedo <matheusq10@gmail.com>
2024-10-16 23:06:59 -0300

    Ajuste: atualizações visuais

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,34 +1,33 @@
+import { useTranslation } from "react-i18next";
+import { BsList } from "react-icons/bs";
+import { HiOutlineHome } from "react-icons/hi";
+import { HiOutlineSparkles } from "react-icons/hi2";
+import { MdCatchingPokemon } from "react-icons/md";
 import { Outlet } from "react-router-dom";
 import { MenuItem } from "../components/Menu/MenuItem";
 import { ThemeToggle } from "../components/Menu/ThemeToggle";
-import { Logo } from "../components/Menu/Logo";
-import { useTranslation } from "react-i18next";
 
 export const MainLayout = () => {
     const { t } = useTranslation()
 
     return (
-        <section className="w-screen min-h-screen flex font-poppins">
-            <aside className="bg-primary w-15v min-w-44 h-100v pt-7 pb-8 border-r border-indigo-400 flex flex-col justify-between">
-                <div>
-                    <Logo />
+        <section className="flex font-poppins">
+            <aside className="flex flex-col justify-between items-center bg-menuback w-5v h-100v pt-7 pb-8 ">
+                <div className="flex flex-col items-center">
+                    <img className="h-auto w-16 rounded mb-2" src="src/assets/logo.jpg" alt="logotipo" />
 
-                    <nav className="mt-11 flex flex-col gap-7 text-gray-50">
-                        <MenuItem icon={"src/assets/pokeball.png"} altText={"Ícone do Início"} text={t("sidebar.home")} to={"/"} />
-                        <MenuItem icon={"src/assets/greatball.png"} altText={"Ícone da Pokédex"} text={"Pokédex"} to={"pokedex"} />
-                        <MenuItem icon={"src/assets/ultraball.png"} altText={"Ícone da Lista de desejos"} text={"Lista de desejos"} to={"listadedesejos"} />
-                        <MenuItem icon={"src/assets/masterball.png"} altText={"Ícone dos Meus shinys"} text={"Meus shinys"} to={"shinys"} />
+                    <nav className="flex flex-col justify-center items-center px-5 mt-14 gap-10 text-gray-50">
+                        <MenuItem icon={HiOutlineHome} altText={"Ícone do Início"} text={t("sidebar.home")} to={"/"} />
+                        <MenuItem icon={MdCatchingPokemon} altText={"Ícone da Pokédex"} text={"Pokédex"} to={"pokedex"} />
+                        <MenuItem icon={BsList} altText={"Ícone da Lista de desejos"} text={"Lista de desejos"} to={"listadedesejos"} />
+                        <MenuItem icon={HiOutlineSparkles} altText={"Ícone dos Meus shinys"} text={"Meus shinys"} to={"shinys"} />
                     </nav>
                 </div>
 
-                <div className="flex flex-col gap-4 text-gray-50 pl-6 pr-6 ">
-                    <hr className="border-stone-300" />
-
-                    <ThemeToggle />
-                </div>
+                <ThemeToggle />
             </aside>
 
-            <main className="flex">
+            <main >
                 <Outlet />
             </main>
         </section>
