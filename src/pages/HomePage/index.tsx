1ec80c462d1dc8aba2636f0df98e7622d8295649
Matheus Quevedo <matheusq10@gmail.com>
2024-10-16 23:06:35 -0300

    Ajuste: atualizações visuais

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -5,17 +5,17 @@ import { FooterPage } from "../../components/FooterPage";
 
 export function HomePage() {
     return (
-        <section className="bg-backcolor w-85v h-100v pt-7 pb-9 pl-10 pr-10 flex flex-1 flex-col justify-between">
+        <section className="bg-gradient-to-tl from-backcolor to-white w-95v h-100v pt-11 pb-9 px-10 flex flex-1 flex-col justify-between">
             <HeaderPage />
 
             <div>
                 <div className="bg-[url('src/assets/Home_banner_degrade.png')] bg-center bg-cover w-full h-35v rounded-lg flex flex-col
                 justify-center truncate">
                     <div>
-                        <p className="text-5xl text-zinc-100 font-bold pl-6">Uma ferramenta para</p>
-                        <p className="text-5xl text-zinc-100 font-bold pl-6">todos os treinadores!</p>
-                        <p className="text-xl text-zinc-100 mt-3 pl-6">Registre seus shinies e escolha os</p>
-                        <p className="text-xl text-zinc-100 mb-6 pl-6">próximos para caçar em um só lugar!</p>
+                        <p className="text-5xl leading-tight text-zinc-100 font-bold pl-6">Uma ferramenta para</p>
+                        <p className="text-5xl leading-tight text-zinc-100 font-bold pl-6">todos os treinadores!</p>
+                        <p className="text-xl leading-normal text-zinc-100 mt-3 pl-6">Registre seus shinies e escolha os</p>
+                        <p className="text-xl leading-normal text-zinc-100 mb-7 pl-6">próximos para caçar em um só lugar!</p>
 
                         <Link
                             to="pokedex"
@@ -27,8 +27,8 @@ export function HomePage() {
                 </div>
             </div>
 
-            <div className="flex gap-12 justify-between">
-                <div className="bg-white w-40v h-fit min-h-52 px-5 py-3 rounded-lg shadow">
+            <div className="flex gap-10 justify-between">
+                <div className="bg-white w-43v h-fit min-h-52 px-5 py-3 rounded-lg shadow">
                     <p className="font-medium text-darkgray">Adicionados recentemente a lista de desejos</p>
                     <div className="flex items-center my-2 pl-4 pb-1 justify-between text-sm border-b-2 text-darkgray">
                         <div className="flex gap-4 items-center">
@@ -59,7 +59,7 @@ export function HomePage() {
                     </Link>
                 </div>
 
-                <div className="bg-white w-40v h-fit min-h-52 px-5 py-3 rounded-lg shadow">
+                <div className="bg-white w-43v h-fit min-h-52 px-5 py-3 rounded-lg shadow">
                     <p className="font-medium text-darkgray">Últimos pokémon capturados</p>
                     <div className="flex items-center my-2 pl-4 pb-1 justify-between text-sm border-b-2 text-darkgray">
                         <div className="flex gap-4 items-center">
@@ -92,17 +92,18 @@ export function HomePage() {
             </div>
 
             <div>
-                <p className="font-bold text-sm mb-2 text-darkgray">Notas de atualização</p>
+                <p className="font-bold text-sm mb-3 text-darkgray">Notas de atualização</p>
 
-                <div className="flex items-center gap-5">
-                    <div className="bg-zinc-300 w-60 h-12"></div>
-                    <div className="bg-zinc-300 w-60 h-12"></div>
-                    <div className="bg-zinc-300 w-60 h-12"></div>
-                    <div className="bg-zinc-300 w-60 h-12"></div>
-                    <div className="bg-zinc-300 w-60 h-12"></div>
-                    <div className="bg-zinc-300 w-60 h-12"></div>
+                <div className="flex items-center justify-between gap-5">
+                    <div className="bg-zinc-300 w-64 h-14 rounded-lg"></div>
+                    <div className="bg-zinc-300 w-64 h-14 rounded-lg"></div>
+                    <div className="bg-zinc-300 w-64 h-14 rounded-lg"></div>
+                    <div className="bg-zinc-300 w-64 h-14 rounded-lg"></div>
+                    <div className="bg-zinc-300 w-64 h-14 rounded-lg"></div>
+                    <div className="bg-zinc-300 w-64 h-14 rounded-lg"></div>
+                    <div className="bg-zinc-300 w-64 h-14 rounded-lg"></div>
                 </div>
-                <p className="text-xs mt-2">*Esse é um projeto brasileiro e sem fins lucratrivos.</p>
+                <p className="text-xs font-medium mt-3">*Esse é um projeto brasileiro e sem fins lucratrivos.</p>
             </div>
 
             <FooterPage />
