528b7d1b1c8c47a0c001eaba1a8917166409d838
Matheus Quevedo <matheusq10@gmail.com>
2024-10-29 20:59:40 -0300

    Mudança: o menu foi atualizado

--- src/components/Menu/MenuItem/index.tsx
+++ src/components/Menu/MenuItem/index.tsx
@@ -1,24 +1,21 @@
 import { Link, LinkProps, useMatch } from "react-router-dom";
 import { cn } from "../../../lib/cn";
+import { IconType } from "react-icons/lib";
 interface MenuItemProps extends LinkProps {
-    icon: string;
+    icon: IconType;
     text: string;
     altText: string;
 }
 
-const ACTIVE_TEXT_COLOR = "text-menucyan font-bold";
-const ACTIVE_BG = "transition ease-in-out delay-50 bg-backcolor/10 pl-5 pr-0 w-56 border-l-4 border-menucyan rounded-tr-lg rounded-br-lg";
+const ACTIVE_BG = "transition ease-in-out bg-neutral-700 rounded-full shadow-lg";
 
-export function MenuItem({ text, icon, altText, to, ...props }: MenuItemProps) {
+export function MenuItem({ text, icon: Icon, altText, to, ...props }: MenuItemProps) {
     const isActive = useMatch(to.toString())
 
     return (
-        <div className={cn("py-2 pl-6 pr-6", { [ACTIVE_BG]: isActive })}>
-            <Link {...props} to={to} className="flex items-center gap-2">
-                <img src={icon} alt={altText} className="h-auto w-9" />
-                <p className={cn("pt-1", { [ACTIVE_TEXT_COLOR]: isActive })}>
-                    {text}
-                </p>
+        <div className={cn("flex item-center justify-center p-2", { [ACTIVE_BG]: isActive })}>
+            <Link {...props} to={to} className="">
+                <Icon aria-label={altText} className="h-auto w-8" />
             </Link>
         </div>
     );
