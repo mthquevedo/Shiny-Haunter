3b2509140a38949f14a3be840c60435d9bed31e9
Matheus Quevedo <matheusq10@gmail.com>
2024-10-29 20:57:19 -0300

    Ajuste: o cabeçalho foi provisóriamente alterado

--- src/components/HeaderPage/index.tsx
+++ src/components/HeaderPage/index.tsx
@@ -2,16 +2,20 @@ import { IoLogoGithub } from "react-icons/io";
 
 export function HeaderPage() {
     return (
-        <div className="flex gap-4 items-center justify-end">
-            <select name="idioma" className="text-primary bg-white text-xs font-medium pl-2 pr-4 py-1 w-32 h-9 rounded-lg
-         border-primary border shadow focus:outline-none cursor-pointer">
-                <option value="portugues" selected>Português</option>
-            </select>
+        <div className="flex gap-4 items-center justify-between">
+            <div className="text-primary font-bold text-2xl">
+                <p>Shiny Haunter</p>
+            </div>
+            <div className="flex gap-4 items-center justify-between">
+                <select name="idioma" className="text-primary bg-white text-xs font-medium pl-2 pr-4 py-1 w-32 h-9 rounded-lg          border-primary border shadow focus:outline-none cursor-pointer">
+                    <option value="portugues" selected>Português</option>
+                </select>
 
-            <a href="https://github.com/mthquevedo/Shiny-Haunter" target="_blank" className="bg-darkgray hover:bg-stone-700 rounded-lg px-4 py-1 h-9 flex items-center justify-center gap-2 shadow">
-                <p className="text-white text-xs font-medium">Envie seus feedbacks!</p>
-                <IoLogoGithub className="fill-white w-6 h-6" />
-            </a>
+                <a href="https://github.com/mthquevedo/Shiny-Haunter" target="_blank" className="bg-darkgray hover:bg-stone-700 rounded-lg px-4 py-1 h-9 flex items-center justify-center gap-2 shadow">
+                    <p className="text-white text-xs font-medium">Envie seus feedbacks!</p>
+                    <IoLogoGithub className="fill-white w-6 h-6" />
+                </a>
+            </div>
         </div>
     );
 }
