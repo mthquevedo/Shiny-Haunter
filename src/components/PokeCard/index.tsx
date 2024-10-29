f837844589a7a223ba6ba8fd67b1fd1181f479c2
Matheus Quevedo <matheusq10@gmail.com>
2024-10-29 20:59:06 -0300

    Atualização: o componente foi atualizado

--- src/components/PokeCard/index.tsx
+++ src/components/PokeCard/index.tsx
@@ -1,59 +1,76 @@
-import { MdBookmarkAdd, MdCatchingPokemon } from "react-icons/md";
+import { MdCatchingPokemon, MdCompare } from "react-icons/md";
+import { Tooltip } from "react-tooltip";
 import { cn } from "../../lib/cn";
-import { TypeFlag } from "../TypeFlag";
 import { bannerTypeColor } from "../../utils/pokemon.utils";
-import { Tooltip } from "react-tooltip"
+import { TypeFlag } from "../TypeFlag";
+import { useState } from "react";
 
 interface PokeCardProps {
     id: number;
     name: string;
-    thumbnail: string;
+    thumbnailDefault: string;
+    thumbnailShiny: string;
     firstType: string;
     secondType: string;
 }
 
-export function PokeCard({ id, name, thumbnail, firstType, secondType }: PokeCardProps) {
+export function PokeCard({ id, name, thumbnailDefault, thumbnailShiny, firstType, secondType }: PokeCardProps) {
+    const [pokeImg, setPokeImg] = useState(thumbnailShiny);
+    const [showDefault, setShowDefault] = useState(false);
+
+    function handleImg() {
+        setShowDefault(!showDefault)
+        showDefault ? setPokeImg(thumbnailShiny) : setPokeImg(thumbnailDefault);
+    }
+
     return (
-        <article className="flex flex-col item bg-white w-14v mt-3 p-3 rounded-2xl shadow-md">
-            <section className={cn("flex flex-col justify-center items-center w-full h-44 p-4 relative overflow-hidden rounded-xl", bannerTypeColor[firstType])}>
-                <p className="absolute top-1 left-2 select-all font-bold text-xl text-neutral-600 mix-blend-multiply">
-                    #{id}
-                </p>
-                <div className="absolute top-1 right-2 flex flex-col gap-2">
+        <article className="flex flex-col item bg-white w-12v mt-3 rounded-2xl shadow-md overflow-hidden">
+            <section className={cn("flex flex-col justify-center items-center w-full h-44 relative overflow-hidden rounded-xl", bannerTypeColor[firstType])}>
+
+                <div className="absolute top-2 w-full px-2 flex items-center justify-between">
+                    <div className="bg-white rounded-full p-1 flex items-center justify-center gap-2 shadow-md">
+                        <TypeFlag type={firstType} />
+                        {secondType ? <TypeFlag type={secondType} /> : ""}
+                    </div>
                     <button
-                        className="flex justify-center items-center text-xl w-8 bg-neutral-300 rounded-lg p-1 hover:bg-red-500 hover:text-white transition"
+                        className="flex justify-center items-center text-xl bg-neutral-400 rounded-full shadow-sm p-1 hover:bg-red-500 active:bg-red-600 text-white transition"
                         data-tooltip-id="toolCapturado"
                         data-tooltip-content="Capturado"
                     >
                         <MdCatchingPokemon />
                     </button>
-                    <Tooltip
+                    {/* <Tooltip
                         id="toolCapturado"
                         style={{ padding: "0.3rem", fontSize: "0.8rem" }}
                         opacity={0.8}
                         place="top"
-                    />
-                    <button className="flex justify-center items-center text-xl w-8 bg-neutral-300 rounded-lg p-1 hover:bg-blue-500 hover:text-white transition">
-                        <MdBookmarkAdd />
-                    </button>
+                    /> */}
                 </div>
+
                 <img
-                    src={thumbnail}
+                    src={pokeImg}
                     alt=""
-                    className="h-44 w-auto mr-1 select-none"
+                    className="h-auto w-40 mt-1 select-none"
                 />
+
             </section>
 
-            <footer className="flex flex-col justify-between items-start mt-3">
-                <div className="flex gap-2">
-                    <TypeFlag type={firstType} />
-                    <TypeFlag type={secondType} />
+            <footer className="flex flex-col justify-between gap-1 px-3 pb-3">
+                <div className="flex justify-between items-center text-lg mt-2 w-full">
+                    <p className="text-neutral-800 font-bold leading-6">{name}</p>
+                    <p className="select-all font-semibold text-neutral-500">
+                        #{id}
+                    </p>
                 </div>
-
-                <div className="flex justify-between items-center mt-2 w-full">
-                    <div className="leading-3">
-                        <p className="text-neutral-800 font-bold text-xl leading-6">{name}</p>
-                    </div>
+                <div className="flex justify-between">
+                    <button
+                        onClick={handleImg}
+                        className="flex justify-center items-center text-lg w-12 border-2 border-neutral-400 text-neutral-700 rounded-lg p-1 hover:bg-neutral-200 hover:text-neutral-600 hover:shadow-sm active:bg-neutral-300 transition">
+                        <MdCompare />
+                    </button>
+                    <button className="flex justify-center w-36 items-center text-xs text-white font-medium bg-blue-500 rounded-lg p-1 hover:bg-blue-400 hover:shadow-sm active:bg-blue-600 transition">
+                        Adicionar
+                    </button>
                 </div>
             </footer>
         </article>
