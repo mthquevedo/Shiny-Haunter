f727039712abf840d6cbad7d271744a9209c427e
Matheus Quevedo <matheusq10@gmail.com>
2024-11-12 23:21:19 -0300

    Ajuste: o novo componente de tooltip foi configurado

--- src/components/Tooltips/CardTooltip/index.tsx
+++ src/components/Tooltips/CardTooltip/index.tsx
@@ -1,19 +1,30 @@
-import { Tooltip, TooltipRefProps } from "react-tooltip";
-import "./CardTooltip.css"
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
 
-interface TooltipProps extends Partial<TooltipRefProps> {
-    id: string;
-}
+interface CardTooltipProps extends Partial<TooltipPrimitive.TooltipProps & TooltipPrimitive.TooltipContentProps> { }
 
-export function CardTooltip({ id, ...props }: TooltipProps) {
+export function CardTooltip({ children, content, open, defaultOpen, onOpenChange, side, ...props }: CardTooltipProps) {
     return (
+        <TooltipPrimitive.Provider delayDuration={0}>
+            <TooltipPrimitive.Root
+                open={open}
+                defaultOpen={defaultOpen}
+                onOpenChange={onOpenChange}
+            >
+                <TooltipPrimitive.Trigger asChild>
+                    {children}
+                </TooltipPrimitive.Trigger>
 
-        <Tooltip
-            {...props}
-            id={id}
-            place={props.place}
-            className="card-tooltip"
-            noArrow
-        />
-    )
+                <TooltipPrimitive.Content
+                    side={side}
+                    align="center"
+                    className="py-1 px-1.5 bg-cardTooltipBg text-xs rounded text-white shadow-md "
+                    {...props}
+                >
+                    {content}
+
+                    <TooltipPrimitive.Arrow className="w-2 h-1 mb-1 fill-cardTooltipBg" />
+                </TooltipPrimitive.Content>
+            </TooltipPrimitive.Root>
+        </TooltipPrimitive.Provider>
+    );
 }
