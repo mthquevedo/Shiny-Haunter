e9b6b71b2ed971d2740484f23aebcb49f062cf27
Matheus Quevedo <matheusq10@gmail.com>
2024-11-17 22:16:02 -0300

    Adição: uma margem foi adicionada abaixo de cada card para igualar ao tamanho do card carregado

--- src/components/LoadingCards/index.tsx
+++ src/components/LoadingCards/index.tsx
@@ -7,7 +7,7 @@ export function LoadingCards() {
     return (
         <div className="flex item-center justify-center flex-wrap gap-5">
             {cards.map((_, i) => (
-                <div key={i} className="bg-slate-200 w-12v h-64 mt-3 rounded-2xl shadow-md overflow-hidden">
+                <div key={i} className="bg-slate-200 w-12v h-64 mt-3 mb-0.5 rounded-2xl shadow-md overflow-hidden">
                     <div className="animate-pulse">
                         <div className="bg-slate-300 flex flex-col justify-center items-center w-full h-44 relative overflow-hidden rounded-xl">
                         </div>
