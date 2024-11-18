92b8f9a88f3d5c91b3dc720d3917fc7bec8c2f64
Matheus Quevedo <matheusq10@gmail.com>
2024-11-17 22:10:26 -0300

    Ajustes: os estilos de botão ativo foram simplificados e um novo estilo para ícone ativo foi criado

--- src/components/Menu/MenuItem/index.tsx
+++ src/components/Menu/MenuItem/index.tsx
@@ -1,22 +1,26 @@
 import { Link, LinkProps, useMatch } from "react-router-dom";
 import { cn } from "../../../lib/cn";
 import { IconType } from "react-icons/lib";
+import { MenuTooltip } from "../../Tooltips/MenuTooltip";
 interface MenuItemProps extends LinkProps {
     icon: IconType;
     text: string;
     altText: string;
 }
 
-const ACTIVE_BG = "transition duration-100 bg-neutral-700 rounded-full shadow-lg";
+const ACTIVE_BG = "hover:bg-neutral-400/50 bg-neutral-400/50 shadow-lg";
+const ACTIVE_ICON = "text-neutral-50";
 
 export function MenuItem({ text, icon: Icon, altText, to, ...props }: MenuItemProps) {
     const isActive = useMatch(to.toString())
 
     return (
-        <div className={cn("flex item-center justify-center p-2", { [ACTIVE_BG]: isActive })}>
-            <Link {...props} to={to} className="">
-                <Icon aria-label={altText} className="h-auto w-8" />
-            </Link>
-        </div>
+        <MenuTooltip content={text} side="right">
+            <div className={cn("transition duration-100 rounded-lg flex item-center justify-center p-2 hover:bg-neutral-400/30  hover:shadow-lg", { [ACTIVE_BG]: isActive })}>
+                <Link {...props} to={to} className="">
+                    <Icon aria-label={altText} className={cn("h-auto w-7 text-neutral-400", { [ACTIVE_ICON]: isActive })} />
+                </Link>
+            </div>
+        </MenuTooltip>
     );
-}
+}   
