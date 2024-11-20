142170302b72d4dbbbd199500874fcf2dc37c254
Matheus Quevedo <matheusq10@gmail.com>
2024-11-19 23:28:11 -0300

    Ajuste: indentação e visual atualizados

--- src/components/Regions/index.tsx
+++ src/components/Regions/index.tsx
@@ -38,14 +38,15 @@ export function RegionsCards() {
         }
     ]
 
-
     return (
-        <div className="flex items-center justify-between px-2 my-2">
+        <div className="flex items-center justify-between my-2">
             {regInfos.map(reg => {
                 return (
-                    <div className="flex items-center justify-between gap-4 bg-slate-700 text-white py-1 px-6 rounded-md">
-                        <p className="bg-white rounded-full text-slate-700 p-1">{reg.pokeNumber}</p>
-                        <p> {reg.name}</p>
+                    <div className="flex items-center gap-4 bg-slate-700 text-white py-1 px-6 rounded-lg">
+                        <p className="bg-white text-slate-700 p-1 w-8 h-8 rounded-full text-center font-medium">
+                            {reg.pokeNumber}
+                        </p>
+                        <p>{reg.name}</p>
                     </div>
                 )
             })}
