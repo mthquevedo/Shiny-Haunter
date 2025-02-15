ef8a97bb524e8476dcd61e6f188a3bcedeb919ff
Matheus Quevedo <matheusq10@gmail.com>
2025-02-15 18:42:43 -0300

    Criação: reducer criado

--- src/store/reducers/catchlist.tsx
+++ src/store/reducers/catchlist.tsx
@@ -1,13 +1,47 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialState = [{
-    teste: "teste"
-}];
+interface CatchlistStateProps {
+    catchlist: string[];
+}
+
+const loadCatchlist = (): string[] => {
+    try {
+        const storedCatchlist = localStorage.getItem("catchlist");
+
+        if (storedCatchlist) {
+            return JSON.parse(storedCatchlist);
+        } else {
+            localStorage.setItem("catchlist", JSON.stringify([]));
+            return [];
+        }
+
+    } catch (error) {
+        console.error("Erro ao carregar a catchlist do localStorage", error);
+        return [];
+    }
+}
+
+const initialState: CatchlistStateProps = {
+    catchlist: loadCatchlist(),
+};
 
 const catchlistSlice = createSlice({
     name: "catchlist",
     initialState,
-    reducers: {}
+    reducers: {
+        toggleCatchlist: (state, action: PayloadAction<string>) => {
+            const pokeName = action.payload;
+
+            if (state.catchlist.includes(pokeName)) {
+                state.catchlist = state.catchlist.filter(name => name !== pokeName);
+            } else {
+                state.catchlist.push(pokeName);
+            }
+
+            localStorage.setItem("catchlist", JSON.stringify(state.catchlist));
+        }
+    }
 });
 
+export const { toggleCatchlist } = catchlistSlice.actions;
 export default catchlistSlice.reducer;
