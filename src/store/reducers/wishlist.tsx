04caf4076c92563d92f703ba39dbe179c31f4040
Matheus Quevedo <matheusq10@gmail.com>
2025-02-14 22:20:56 -0300

    Criação: lógica de reducer criada

--- src/store/reducers/wishlist.tsx
+++ src/store/reducers/wishlist.tsx
@@ -1,13 +1,46 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialState = [{
-    teste: "teste"
-}];
+interface WishlistStateProps {
+    wishlist: string[];
+}
+
+const loadWishlist = (): string[] => {
+    try {
+        const storedWishlist = localStorage.getItem("wishlist");
+
+        if (storedWishlist) {
+            return JSON.parse(storedWishlist);
+        } else {
+            localStorage.setItem("wishlist", JSON.stringify([]));
+            return [];
+        }
+    } catch (error) {
+        console.error("Erro ao carregar a wishlist do localStorage", error);
+        return [];
+    }
+}
+
+const initialState: WishlistStateProps = {
+    wishlist: loadWishlist(),
+};
 
 const wishlistSlice = createSlice({
     name: "wishlist",
     initialState,
-    reducers: {}
+    reducers: {
+        toggleWishlist: (state, action: PayloadAction<string>) => {
+            const pokeName = action.payload;
+
+            if (state.wishlist.includes(pokeName)) {
+                state.wishlist = state.wishlist.filter(name => name !== pokeName);
+            } else {
+                state.wishlist.push(pokeName);
+            }
+
+            localStorage.setItem("wishlist", JSON.stringify(state.wishlist));
+        }
+    }
 });
 
+export const { toggleWishlist } = wishlistSlice.actions;
 export default wishlistSlice.reducer;
