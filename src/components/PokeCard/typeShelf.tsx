d74a2eb655c7886f4d74cf3ba51fd1d4105307ac
Matheus Quevedo <matheusq10@gmail.com>
2025-02-14 22:18:42 -0300

    Ajuste: a flag de tipagem recebeu uma key

--- src/components/PokeCard/typeShelf.tsx
+++ src/components/PokeCard/typeShelf.tsx
@@ -8,7 +8,7 @@ interface TypeShelfProps {
 export function TypeShelf({ types }: TypeShelfProps) {
     return (
         <div className="bg-white rounded-full p-1 flex items-center justify-center gap-1 shadow-md">
-            {types.map(type => <TypeFlag type={type.type.name} />)}
+            {types.map(type => <TypeFlag key={type.slot} type={type.type.name} />)}
         </div>
     );
 }
