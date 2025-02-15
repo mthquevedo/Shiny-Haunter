c057911a49bfcac962300f3c3084144d63e56131
Matheus Quevedo <matheusq10@gmail.com>
2025-02-14 22:19:57 -0300

    Criação: a lógica do componente foi criada

--- src/components/PokeCard/wishButton.tsx
+++ src/components/PokeCard/wishButton.tsx
@@ -1,12 +1,31 @@
-import { CardTooltip } from "../Tooltips/CardTooltip";
 import { MdCatchingPokemon } from "react-icons/md";
+import { useDispatch, useSelector, } from "react-redux";
+import { cn } from "../../lib/cn";
+import { RootState } from "../../store";
+import { toggleWishlist } from "../../store/reducers/wishlist";
+import { CardTooltip } from "../Tooltips/CardTooltip";
 
-export function WishButton() {
+const ACTIVE_BUTTON = "bg-red-500";
+
+interface WishButtonProps {
+    pokeName: string;
+}
+
+export function WishButton({ pokeName }: WishButtonProps) {
+    const dispatch = useDispatch();
+    const wishlist = useSelector((state: RootState) => state.wishlist.wishlist);
+
+    const isWished = wishlist.includes(pokeName);
+
+    const handleToggle = () => {
+        dispatch(toggleWishlist(pokeName));
+    };
     return (
         <>
             <CardTooltip content="Capturado" side="top">
                 <button
-                    className="flex justify-center items-center text-xl bg-neutral-400 rounded-full shadow-sm p-1 hover:bg-red-500 active:bg-red-600 text-white transition"
+                    className={cn("flex justify-center items-center text-xl bg-neutral-400 rounded-full shadow-sm p-1 hover:bg-red-500 active:bg-red-600 text-white transition", { [ACTIVE_BUTTON]: isWished })}
+                    onClick={handleToggle}
                 >
                     <MdCatchingPokemon />
                 </button>
