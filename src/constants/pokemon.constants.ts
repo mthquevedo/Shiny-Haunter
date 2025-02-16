42f1ff5d2bd427284f37a6f2669a24409be0e80d
Matheus Quevedo <matheusq10@gmail.com>
2025-02-15 21:27:14 -0300

    Adição: interface de props do pokemon adicionada

--- src/constants/pokemon.constants.ts
+++ src/constants/pokemon.constants.ts
@@ -1,1 +1,11 @@
+import { PokemonType } from "pokenode-ts";
+
 export const LIMIT_CARDS = 14;
+
+export interface PokeCardProps {
+    id?: number;
+    name?: string;
+    thumbnailDefault?: string;
+    thumbnailShiny?: string;
+    types?: PokemonType[];
+}
