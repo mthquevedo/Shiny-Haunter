88bd2c4dd4ca78498f2fb014588493746cde1499
Matheus Quevedo <matheusq10@gmail.com>
2025-02-15 21:28:44 -0300

    Ajuste: mudanças para adpatar o componente a mudanças de estado

--- src/components/PokeCard/typeShelf.tsx
+++ src/components/PokeCard/typeShelf.tsx
@@ -1,14 +1,15 @@
-import { PokemonType } from "pokenode-ts";
+import { PokeCardProps } from "../../constants/pokemon.constants";
 import { TypeFlag } from "../TypeFlag";
 
-interface TypeShelfProps {
-    types: PokemonType[];
-}
 
-export function TypeShelf({ types }: TypeShelfProps) {
+// interface TypeShelfProps {
+//     types: PokemonType[];
+// }
+
+export function TypeShelf({ types }: PokeCardProps) {
     return (
         <div className="bg-white rounded-full p-1 flex items-center justify-center gap-1 shadow-md">
-            {types.map(type => <TypeFlag key={type.slot} type={type.type.name} />)}
+            {types?.map(type => <TypeFlag key={type.slot} type={type.type.name} />)}
         </div>
     );
 }
