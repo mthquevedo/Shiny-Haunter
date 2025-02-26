47a706299b6c795f841850f002e59d7bc2dc4882
Matheus Quevedo <matheusq10@gmail.com>
2025-02-25 23:53:55 -0300

    Ajuste: o conteúdo foi ajustado

--- src/components/FooterPage/index.tsx
+++ src/components/FooterPage/index.tsx
@@ -2,7 +2,7 @@ export function FooterPage() {
     return (
         <div className="text-xs flex items-center justify-between text-darkgray">
             <p className="font-medium">Versão 1.0</p>
-            <p className="font-bold">Criador por <a href="https://github.com/mthquevedo" target="_blank" className="text-primary">Matheus Quevedo</a></p>
+            <p className="font-bold">Criador por <a href="https://github.com/mthquevedo" target="_blank" className="text-primary">@Matheus Quevedo</a></p>
         </div>
     );
 }
