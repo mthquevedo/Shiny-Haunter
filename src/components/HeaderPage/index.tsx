20ce223e76ea762362d2897c049065c811488c0d
Matheus Quevedo <matheusq10@gmail.com>
2025-03-29 22:01:05 -0300

    Criação: novo elemento de select criado

--- src/components/HeaderPage/index.tsx
+++ src/components/HeaderPage/index.tsx
@@ -1,15 +1,59 @@
+import { Select } from "radix-ui";
+import { useState } from "react";
 import { IoLogoGithub } from "react-icons/io";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 
 export function HeaderPage() {
+    const [value, setValue] = useState("Idioma");
+
     return (
         <div className="flex gap-4 items-center justify-between">
             <div className="text-primary font-bold text-2xl">
                 <p>Shiny Haunter</p>
             </div>
             <div className="flex gap-4 items-center justify-between">
-                <select name="idioma" className="text-primary bg-white text-xs font-medium pl-2 pr-4 py-1 w-32 h-9 rounded-lg          border-primary border shadow focus:outline-none cursor-pointer">
-                    <option value="portugues" selected>Português</option>
-                </select>
+                <Select.Root value={value} onValueChange={setValue}>
+                    <Select.Trigger className="flex items-center justify-between gap-2 text-primary bg-white text-xs font-medium px-4 py-1 w-32 h-9 rounded-lg border-primary border shadow focus:outline-none cursor-pointer">
+                        <Select.Value>
+                            {value}
+                        </Select.Value>
+                        <Select.Icon>
+                            <MdOutlineKeyboardArrowDown className="text-xl" />
+                        </Select.Icon>
+                    </Select.Trigger>
+
+                    <Select.Portal>
+                        <Select.Content
+                            position="popper"
+                            className="overflow-hidden w-32 bg-white shadow rounded-lg text-sm"
+                        >
+                            <Select.Viewport className="p-2">
+                                <Select.Group>
+                                    <Select.Item value="Português" className="cursor-pointer text-menuback focus:outline-none p-px rounded-sm hover:bg-neutral-100">
+                                        <Select.ItemText>
+                                            Português
+                                        </Select.ItemText>
+                                        <Select.ItemIndicator />
+                                    </Select.Item>
+                                    <Select.Separator className="h-px my-1 bg-slate-300" />
+                                    <Select.Item value="Inglês" disabled className="cursor-no-drop text-neutral-400">
+                                        <Select.ItemText>
+                                            Inglês
+                                        </Select.ItemText>
+                                        <Select.ItemIndicator />
+                                    </Select.Item>
+                                    <Select.Separator className="h-px my-1 bg-slate-300" />
+                                    <Select.Item value="Espanhol" disabled className="cursor-no-drop text-neutral-400">
+                                        <Select.ItemText>
+                                            Espanhol
+                                        </Select.ItemText>
+                                        <Select.ItemIndicator />
+                                    </Select.Item>
+                                </Select.Group>
+                            </Select.Viewport>
+                        </Select.Content>
+                    </Select.Portal>
+                </Select.Root>
 
                 <a href="https://github.com/mthquevedo/Shiny-Haunter" target="_blank" className="bg-darkgray hover:bg-stone-700 rounded-lg px-4 py-1 h-9 flex items-center justify-center gap-2 shadow">
                     <p className="text-white text-xs font-medium">Envie seus feedbacks!</p>
