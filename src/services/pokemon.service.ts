1605108648a0ea4d3e539ac8fec49c6be844baec
Matheus Quevedo <matheusq10@gmail.com>
2025-04-03 21:52:09 -0300

    Criação: serviço refatorado e com novo método

--- src/services/pokemon.service.ts
+++ src/services/pokemon.service.ts
@@ -1,13 +1,21 @@
 import { pokemonClient } from "./pokemon.client";
 
 const LIMIT = 10;
+class PokemonServices {
+    async getLimitedPokemons(offset = 0, limit = LIMIT) {
+        const { results, ...pagination } = await pokemonClient.listPokemons(offset, limit);
 
-export async function getPokemons(offset = 0, limit = LIMIT) {
-    const { results, ...pagination } = await pokemonClient.listPokemons(offset, limit);
+        const pokemons = await Promise.all(
+            results.map(({ name }) => pokemonClient.getPokemonByName(name))
+        );
 
-    const pokemons = await Promise.all(
-        results.map(({ name }) => pokemonClient.getPokemonByName(name))
-    );
+        return { pokemons, ...pagination };
+    }
 
-    return { pokemons, ...pagination };
+    async getAllPokemons(offset = 0, limit = 1302): Promise<string[]> {
+        const { results } = await pokemonClient.listPokemons(offset, limit);
+        return results.map(pokemon => pokemon.name);
+    }
 }
+
+export const pokemonServices = new PokemonServices();
