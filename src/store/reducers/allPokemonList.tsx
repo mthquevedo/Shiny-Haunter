8593bcf970335513abb181341321f9334bf61e03
Matheus Quevedo <matheusq10@gmail.com>
2025-04-06 23:29:53 -0300

    Criação: as lógicas de initialState e reducers foram criadas

--- src/store/reducers/allPokemonList.tsx
+++ src/store/reducers/allPokemonList.tsx
@@ -1,11 +1,45 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState: string[] = [];
+interface pokemonListProps {
+    list: string[];
+    loading: boolean;
+}
+
+const loadPokemonList = (): string[] => {
+    try {
+        const storedList = localStorage.getItem("pokelist");
+
+        if (storedList) {
+            return JSON.parse(storedList);
+        } else {
+            localStorage.setItem("pokelist", JSON.stringify([]));
+            return [];
+        }
+    } catch (error) {
+        console.error("Erro ao carregar a lista de pokemon do localStorage", error);
+        return [];
+    }
+};
+
+const initialState: pokemonListProps = {
+    list: loadPokemonList(),
+    loading: false,
+};
+
 
 const allPokemonListSlice = createSlice({
     name: "allPokemonList",
     initialState,
-    reducers: {}
+    reducers: {
+        createList: (state, { payload }) => {
+            state.list.push(payload);
+            localStorage.setItem("pokelist", JSON.stringify(payload));
+        },
+        setLoading: (state, { payload }) => {
+            state.loading = payload;
+        }
+    }
 });
 
+export const { createList, setLoading } = allPokemonListSlice.actions;
 export default allPokemonListSlice.reducer;
