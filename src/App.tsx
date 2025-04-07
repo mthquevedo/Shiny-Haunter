3f4b8bffbffbe112f2680a0a603c5e02e7774ee5
Matheus Quevedo <matheusq10@gmail.com>
2025-04-06 23:26:15 -0300

    Criação: lógica para acionar a requisição de todos os pokemons criada

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
-import { MainLayout } from "./layouts/MainLayout"
-import { HomePage } from "./pages/HomePage"
-import { Pokedex } from "./pages/Pokedex"
-import { Shinys } from "./pages/Shinys"
-import { Wishlist } from "./pages/Wishlist"
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { MainLayout } from "./layouts/MainLayout";
+import { HomePage } from "./pages/HomePage";
+import { usePokeList } from "./pages/HomePage/home.hook";
+import { Pokedex } from "./pages/Pokedex";
+import { Shinys } from "./pages/Shinys";
+import { Wishlist } from "./pages/Wishlist";
+import { RootState } from "./store";
+
 function App() {
+  const { fetchAllPokemons } = usePokeList();
+  const pokemonList = useSelector((state: RootState) => state.allPokemonList.list);
+
+  useEffect(() => {
+    if (pokemonList.length === 0) {
+      fetchAllPokemons();
+    }
+  }, [])
 
   return (
     <BrowserRouter>
