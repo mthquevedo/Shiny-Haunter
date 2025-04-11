2a4c980749171896c5891beb15c8756fd32385ec
Matheus Quevedo <matheusq10@gmail.com>
2025-04-11 20:37:40 -0300

    Adição: novo reducer adicionado

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import wishlistSlice from "./reducers/wishlist";
 import catchlistSlice from "./reducers/catchlist";
 import allPokemonListSlice from "./reducers/allPokemonList";
+import searchBarSlice from "./reducers/searchBar";
 
 const store = configureStore({
     reducer: {
         wishlist: wishlistSlice,
         catchlist: catchlistSlice,
         allPokemonList: allPokemonListSlice,
+        searchBar: searchBarSlice,
     }
 });
 
