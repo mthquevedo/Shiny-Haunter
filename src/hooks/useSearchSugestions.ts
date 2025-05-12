4b8e627007bf535ec79f51c2e0151a085fffed32
mthquevedo <matheusq10@gmail.com>
2025-05-12 19:29:25 -0300

    Criação: hook para gestão de sugestões

---
+++ src/hooks/useSearchSugestions.ts
@@ -0,0 +1,19 @@
+import { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../store";
+import { setSugestionsList } from "../store/reducers/searchSugetions";
+
+export function useSearchSugestions() {
+    const dispatch = useDispatch();
+    const pokemonList = useSelector((state: RootState) => state.allPokemonList.list);
+
+    const handleSugestionsList = useCallback((searchValue: string) => {
+            const filteredPokemonList = pokemonList.filter(pokeName =>
+                pokeName.label.toLocaleLowerCase().startsWith(searchValue.toLocaleLowerCase())
+            );
+            dispatch(setSugestionsList(filteredPokemonList));
+    }, [pokemonList, dispatch]);
+
+
+    return { handleSugestionsList };
+}
