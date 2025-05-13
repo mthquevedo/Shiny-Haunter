f7af51c258b51996b072ac412525be42a55f3e7a
mthquevedo <matheusq10@gmail.com>
2025-05-13 19:06:30 -0300

    Adição: novos states e actions foram criados

--- src/store/reducers/pokedexList.tsx
+++ src/store/reducers/pokedexList.tsx
@@ -1,14 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit"
 import { Pokemon } from "pokenode-ts";
+import { LIMIT_CARDS } from "../../constants/pokemon.constants";
 
 interface pokedexStateProps {
     list: Pokemon[],
-    loading: boolean
+    loading: boolean,
+    offset: number,
+    next: boolean,
+    previous: boolean
 }
 
 const initialState: pokedexStateProps = {
     list: [],
-    loading: false
+    loading: false,
+    offset: 0,
+    next: false,
+    previous: false
 };
 
 const pokedexListSlice = createSlice({
@@ -18,6 +25,21 @@ const pokedexListSlice = createSlice({
         setList: (state, { payload }) => {
             state.list = payload;
         },
+        handleNextPage: (state) => {
+            state.offset = state.offset + LIMIT_CARDS;
+        },
+        handlePreviousPage: (state) => {
+            if (state.offset === 0) {
+                return;
+            }
+            state.offset = state.offset - LIMIT_CARDS;
+        },
+        setPrevious: (state, { payload }) => {
+            state.previous = payload;
+        },
+        setNext: (state, { payload }) => {
+            state.next = payload;
+        },
         setLoading: (state, { payload }) => {
             state.loading = payload;
         }
@@ -25,5 +47,12 @@ const pokedexListSlice = createSlice({
     }
 })
 
-export const { setList, setLoading } = pokedexListSlice.actions;
+export const {
+    setList,
+    handleNextPage,
+    handlePreviousPage,
+    setLoading,
+    setPrevious,
+    setNext
+} = pokedexListSlice.actions;
 export default pokedexListSlice.reducer;
