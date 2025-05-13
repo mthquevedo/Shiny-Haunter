3483c92de479e547f47dd35646d39ec2c62e49c3
mthquevedo <matheusq10@gmail.com>
2025-05-13 19:07:51 -0300

    Ajuste: header e footer removidos

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,31 +1,25 @@
 
 import { Link } from "react-router-dom";
-import { FooterPage } from "../../components/FooterPage";
-import { HeaderPage } from "../../components/HeaderPage";
 import { LastCatched } from "../../components/LastCatched";
 import { LastWished } from "../../components/LastWished";
 
 export function HomePage() {
     return (
-        <section className="bg-gradient-to-tl from-backcolor to-white w-95v h-100v pt-11 pb-9 px-10 flex flex-1 flex-col justify-between">
-            <HeaderPage />
-
-            <div>
-                <div className="bg-[url('src/assets/Home_banner_degrade.png')] bg-center bg-cover w-full h-35v rounded-lg flex flex-col
+        <>
+            <div className="bg-[url('src/assets/Home_banner_degrade.png')] bg-center bg-cover w-full h-35v rounded-lg flex flex-col
                 justify-center truncate">
-                    <div>
-                        <p className="text-5xl leading-tight text-zinc-100 font-bold pl-6">Uma ferramenta para</p>
-                        <p className="text-5xl leading-tight text-zinc-100 font-bold pl-6">todos os treinadores!</p>
-                        <p className="text-xl leading-normal text-zinc-100 mt-3 pl-6">Registre seus shinies e escolha os</p>
-                        <p className="text-xl leading-normal text-zinc-100 mb-7 pl-6">próximos para caçar em um só lugar!</p>
+                <div>
+                    <p className="text-5xl leading-tight text-zinc-100 font-bold pl-6">Uma ferramenta para</p>
+                    <p className="text-5xl leading-tight text-zinc-100 font-bold pl-6">todos os treinadores!</p>
+                    <p className="text-xl leading-normal text-zinc-100 mt-3 pl-6">Registre seus shinies e escolha os</p>
+                    <p className="text-xl leading-normal text-zinc-100 mb-7 pl-6">próximos para caçar em um só lugar!</p>
 
-                        <Link
-                            to="pokedex"
-                            className="group text-lg bg-gradient-to-r from-buttoncyan to-indigo-500 from-30% px-4 py-2
+                    <Link
+                        to="pokedex"
+                        className="group text-lg bg-gradient-to-r from-buttoncyan to-indigo-500 from-30% px-4 py-2
                          text-white ml-6 rounded-lg shadow-lg transition-all ease-in-out hover:brightness-110 active:scale-95">
-                            Comece agora!
-                        </Link>
-                    </div>
+                        Comece agora!
+                    </Link>
                 </div>
             </div>
 
@@ -49,8 +43,6 @@ export function HomePage() {
                 </div>
                 <p className="text-xs font-medium mt-3">*Projeto sem fins lucratrivos.</p>
             </div>
-
-            <FooterPage />
-        </section>
+        </>
     );
 }
