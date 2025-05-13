09cefc6a414005c23baed00427495dee207dfa11
mthquevedo <matheusq10@gmail.com>
2025-05-13 16:35:15 -0300

    Adição: validação de dados ao renderizar

--- src/components/PokeCardsArea/index.tsx
+++ src/components/PokeCardsArea/index.tsx
@@ -4,12 +4,21 @@ import { RootState } from "../../store";
 import { idValidation, imageValidation, nameValidation, typeValidation } from "../../utils/cardValidation";
 import { LoadingCards } from "../LoadingCards";
 import { PokeCard } from "../PokeCard";
+import { useEffect } from "react";
+import { usePokedex } from "../../hooks/usePokedex";
 
 export function PokeCardsArea() {
     const isPageLoading = useSelector((state: RootState) => state.pokedexList.loading);
     const pokemonList = useSelector((state: RootState) => state.pokedexList.list);
     const searchList = useSelector((state: RootState) => state.searchPokedex.searchList);
     const cardsQuantity = searchList.length === 0 ? LIMIT_CARDS : searchList.length;
+    const { getPokemonPage } = usePokedex();
+
+    useEffect(() => {
+        if (pokemonList.length !== LIMIT_CARDS) {
+            getPokemonPage();
+        }
+    }, []);
 
     return (
         <div className="w-full h-full flex items-start justify-start flex-wrap gap-5">
