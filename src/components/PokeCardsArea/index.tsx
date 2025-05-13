42448e19ba48c11be01107d5824150061fc66fe3
mthquevedo <matheusq10@gmail.com>
2025-05-13 16:23:11 -0300

    Criação: componente de exibição dos cards

---
+++ src/components/PokeCardsArea/index.tsx
@@ -0,0 +1,36 @@
+import { useSelector } from "react-redux";
+import { LIMIT_CARDS } from "../../constants/pokemon.constants";
+import { RootState } from "../../store";
+import { idValidation, imageValidation, nameValidation, typeValidation } from "../../utils/cardValidation";
+import { LoadingCards } from "../LoadingCards";
+import { PokeCard } from "../PokeCard";
+
+export function PokeCardsArea() {
+    const isPageLoading = useSelector((state: RootState) => state.pokedexList.loading);
+    const pokemonList = useSelector((state: RootState) => state.pokedexList.list);
+    const searchList = useSelector((state: RootState) => state.searchPokedex.searchList);
+    const cardsQuantity = searchList.length === 0 ? LIMIT_CARDS : searchList.length;
+
+    return (
+        <div className="w-full h-full flex items-start justify-start flex-wrap gap-5">
+            {isPageLoading ?
+                <LoadingCards quantity={cardsQuantity} />
+                :
+                <>
+                    {pokemonList.map(pokemon => {
+                        return (
+                            <PokeCard
+                                id={idValidation(pokemon.id)}
+                                key={pokemon.id + pokemon.name}
+                                name={nameValidation(pokemon.name)}
+                                thumbnailDefault={imageValidation(pokemon.sprites?.other?.["official-artwork"]?.front_default)}
+                                thumbnailShiny={imageValidation(pokemon.sprites?.other?.["official-artwork"]?.front_shiny)}
+                                types={typeValidation(pokemon.types)}
+                            />
+                        )
+                    })}
+                </>
+            }
+        </div>
+    )
+}
