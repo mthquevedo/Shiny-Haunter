e18d317b5917446ad95a11186c5cfb3edab4ad8f
mthquevedo <matheusq10@gmail.com>
2025-05-14 19:10:32 -0300

    Criação: layput base da página criado

--- src/pages/Shinys/index.tsx
+++ src/pages/Shinys/index.tsx
@@ -1,3 +1,16 @@
+import { IntroArea } from "../../components/introArea";
+
 export function Shinys() {
-    return ("")
+    return (
+        <>
+            <IntroArea
+                title="Meus Shinys"
+                description="Adicione os pokémon às listas de desejos ou capturados, caso prefira, busque pelo nome específico na barra de pesquisa."
+            />
+
+            <section className="flex flex-col justify-around gap-5 w-full h-75v">
+
+            </section>
+        </>
+    )
 }
