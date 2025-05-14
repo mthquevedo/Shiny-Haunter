46bbf840d6b32bbd64af864262cbafc1cb722791
mthquevedo <matheusq10@gmail.com>
2025-05-14 19:10:27 -0300

    Criação: layput base da página criado

--- src/pages/Wishlist/index.tsx
+++ src/pages/Wishlist/index.tsx
@@ -1,3 +1,16 @@
+import { IntroArea } from "../../components/introArea";
+
 export function Wishlist() {
-    return ("")
+    return (
+        <>
+            <IntroArea
+                title="Lista de desejos"
+                description="Adicione os pokémon às listas de desejos ou capturados, caso prefira, busque pelo nome específico na barra de pesquisa."
+            />
+
+            <section className="flex flex-col justify-around gap-5 w-full h-75v">
+             
+            </section>
+        </>
+    )
 }
