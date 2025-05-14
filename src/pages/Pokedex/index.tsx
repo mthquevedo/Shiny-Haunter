641e63afd5b233846c287667efc159bfdaf23760
mthquevedo <matheusq10@gmail.com>
2025-05-14 19:11:55 -0300

    Ajuste: componente renomeado

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -1,13 +1,14 @@
 import { PaginationButtons } from "../../components/paginationButtons";
 import { PokeCardsArea } from "../../components/PokeCardsArea";
-import { SearchArea } from "../../components/SearchArea";
+import { IntroArea } from "../../components/introArea";
 
 export function Pokedex() {
     return (
         <>
-            <SearchArea
+            <IntroArea
                 title="Pokédex"
                 description="Adicione os pokémon às listas de desejos ou capturados, caso prefira, busque pelo nome específico na barra de pesquisa."
+                hasSearch
             />
 
             <section className="flex flex-col justify-around gap-5 w-full h-75v">
