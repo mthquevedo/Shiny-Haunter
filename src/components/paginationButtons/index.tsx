e8f79fc3f19534697cad4a68e454be50cd9419f6
mthquevedo <matheusq10@gmail.com>
2025-05-15 19:55:52 -0300

    Ajuste: hook de paginação incorporado no componente

--- src/components/paginationButtons/index.tsx
+++ src/components/paginationButtons/index.tsx
@@ -1,11 +1,11 @@
-import { useDispatch, useSelector } from "react-redux"
-import { RootState } from "../../store"
-import { handleNextPage, handlePreviousPage } from "../../store/reducers/pokedexList";
+import { useSelector } from "react-redux";
 import { LIMIT_CARDS } from "../../constants/pokemon.constants";
+import { usePagination } from "../../hooks/usePagination";
+import { RootState } from "../../store";
 
 export function PaginationButtons() {
-    const dispatch = useDispatch();
     const { next, previous, list } = useSelector((state: RootState) => state.pokedexList);
+    const { nextPage, previousPage } = usePagination();
 
     return (
         <>
@@ -14,14 +14,14 @@ export function PaginationButtons() {
                 :
                 <div className="flex gap-4 items-center justify-center">
                     <button
-                        onClick={() => dispatch(handlePreviousPage())}
+                        onClick={() => previousPage()}
                         disabled={!previous}
                         className="bg-primary hover:bg-indigo-800 active:bg-indigo-900 disabled:bg-slate-400 disabled:cursor-not-allowed disabled:select-none text-white rounded-md py-2 px-5 disabled:active:scale-100 active:scale-[0.98] font-medium text-sm shadow-md transition"
                     >
                         Anterior
                     </button>
                     <button
-                        onClick={() => dispatch(handleNextPage())}
+                        onClick={() => nextPage()}
                         disabled={!next}
                         className="bg-primary hover:bg-indigo-800 active:bg-indigo-900 disabled:bg-slate-400 disabled:cursor-not-allowed disabled:select-none text-white rounded-md py-2 px-4 disabled:active:scale-100 active:scale-[0.98] font-medium text-sm shadow-md transition"
                     >
