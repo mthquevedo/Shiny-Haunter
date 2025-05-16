105eadd32c64c7aa9c45d3ed6390122b02d1bdff
mthquevedo <matheusq10@gmail.com>
2025-05-16 18:41:13 -0300

    Remoção: a validação de imagem deixou de ser necessária

--- src/utils/cardValidation.ts
+++ src/utils/cardValidation.ts
@@ -9,10 +9,6 @@ export function nameValidation(name: string) {
     return name ? upperFirstLetter(name) : "???";
 }
 
-export function imageValidation(img: string | undefined | null) {
-    return typeof img === "string" ? img : "src/assets/pokeball-icon-11.png";
-}
-
 export function typeValidation(type: PokemonType[] = []) {
     return type.length > 0 ? type : defaultType;
 }
