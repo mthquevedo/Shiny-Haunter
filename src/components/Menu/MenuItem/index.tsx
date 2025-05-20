8f992ac1dc88121241c0445c5543ff7b7c428175
mthquevedo <matheusq10@gmail.com>
2025-05-19 21:22:23 -0300

    Remoção: tooltip removido

--- src/components/Menu/MenuItem/index.tsx
+++ src/components/Menu/MenuItem/index.tsx
@@ -1,7 +1,6 @@
+import { IconType } from "react-icons/lib";
 import { Link, LinkProps, useMatch } from "react-router-dom";
 import { cn } from "../../../lib/cn";
-import { IconType } from "react-icons/lib";
-import { MenuTooltip } from "../../Tooltips/MenuTooltip";
 interface MenuItemProps extends LinkProps {
     icon: IconType;
     text: string;
@@ -9,18 +8,19 @@ interface MenuItemProps extends LinkProps {
 }
 
 const ACTIVE_BG = "hover:bg-neutral-400/30 bg-neutral-400/30 border-2 border-neutral-600 shadow-lg";
+const ACTIVE_TEXT = "font-medium";
 const ACTIVE_ICON = "text-neutral-50";
 
 export function MenuItem({ text, icon: Icon, altText, to, ...props }: MenuItemProps) {
     const isActive = useMatch(to.toString())
 
     return (
-        <MenuTooltip content={text} side="right">
-            <Link {...props} to={to}>
-                <div className={cn("transition rounded-lg flex item-center justify-center p-2.5 border-2 border-menuback hover:bg-neutral-400/30 hover:shadow-lg active:scale-95", { [ACTIVE_BG]: isActive })}>
-                    <Icon aria-label={altText} className={cn("h-auto w-6 text-neutral-400", { [ACTIVE_ICON]: isActive })} />
-                </div>
-            </Link>
-        </MenuTooltip>
+        <Link {...props} to={to}>
+            <div className={cn("flex flex-col items-center justify-center gap-1.5 w-16 h-16 border-2 border-menuback hover:bg-neutral-400/30 hover:shadow-lg active:scale-95 transition duration-100 rounded-lg", { [ACTIVE_BG]: isActive })}>
+                <Icon aria-label={altText} className={cn("h-auto w-7 text-neutral-400", { [ACTIVE_ICON]: isActive })} />
+
+                <span className={cn("text-xs text-neutral-300 transition", { [ACTIVE_TEXT]: isActive })}>{text}</span>
+            </div>
+        </Link>
     );
 }   
