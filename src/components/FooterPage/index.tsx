63246e8331f1805330673b008e27cd4f163ad0ea
mthquevedo <matheusq10@gmail.com>
2025-05-19 21:21:32 -0300

    Ajuste: condicional de aviso configurada

--- src/components/FooterPage/index.tsx
+++ src/components/FooterPage/index.tsx
@@ -1,8 +1,14 @@
+import { useMatch } from "react-router-dom";
+
 export function FooterPage() {
+    const isHomePage = useMatch("/");
+
     return (
-        <div className="text-xs flex items-center justify-between text-darkgray">
-            <p className="font-medium">Versão 1.0</p>
-            <p className="font-bold">Criador por <a href="https://github.com/mthquevedo" target="_blank" className="text-primary">@Matheus Quevedo</a></p>
+        <div className="text-xs w-full flex items-center justify-between text-darkgray">
+            {isHomePage && (
+                <p className="font-medium mt-3">*Projeto sem fins lucratrivos.</p>
+            )}
+            <p className="flex-1 text-right font-bold">Criador por <a href="https://github.com/mthquevedo" target="_blank" className="text-primary">@Matheus Quevedo</a></p>
         </div>
     );
 }
