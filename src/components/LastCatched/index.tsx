23425672bdf5e77ac3d7bf4000d8bc843ca36f8f
mthquevedo <matheusq10@gmail.com>
2025-05-20 17:34:51 -0300

    Ajuste: layout atualizado para transmitir clareza

--- src/components/LastCatched/index.tsx
+++ src/components/LastCatched/index.tsx
@@ -8,26 +8,37 @@ export function LastCatched() {
     const lastCatched = catchlist.slice(-3)
 
     return (
-        <div className="bg-white w-43v h-28v px-5 py-3 rounded-lg shadow">
+        <div className="flex flex-col justify-between bg-white w-43v h-28v px-5 py-4 rounded-lg shadow">
             {lastCatched[0] ?
                 <>
-                    <p className="font-medium text-darkgray">Últimos pokémon capturados</p>
+                    <header className="flex items-center justify-between mb-1">
+                        <h2 className="font-medium text-darkgray">Últimos pokémon capturados</h2>
+                        <Link to="shinys" className="py-1 px-2 font-medium text-xs text-zinc-800 rounded-md border border-zinc-300  hover:bg-gray-200 transition hover:shadow-sm ">
+                            <p>Ver todos</p>
+                        </Link>
+                    </header>
 
-                    {lastCatched.map(item => {
-                        return (
-                            <div key={item.name} className="flex items-center my-2 pl-4 pb-1 justify-between text-sm border-b-2 text-darkgray">
-                                <div className="flex gap-4 items-center">
-                                    <img src={item.image} alt="Foto de pokémon" className="w-12 h-12" />
-                                    <p>{item.name}</p>
-                                </div>
+                    <ul className="flex-1">
+                        {lastCatched.map(item => {
+                            return (
+                                <li
+                                    key={item.name}
+                                    className="flex items-center bg-indigo-50 my-3 px-4 justify-between text-sm rounded-md text-darkgray"
+                                >
+                                    <div className="flex gap-4 items-center">
+                                        <img src={item.image} alt={`Imagem do pokémon ${item.name}`} className="w-12 h-12" />
 
-                                <p>{item.date}</p>
-                            </div>
-                        )
-                    })}
-                    <Link to="shinys" className="text-xs font-bold text-primary text-right pt-1 hover:underline">
-                        <p>Ver lista completa</p>
-                    </Link>
+                                        <p className="font-medium">{item.name}</p>
+                                    </div>
+
+                                    <div className="flex flex-col gap-1 text-right">
+                                        <p className="text-xs font-medium">Adicionado em:</p>
+                                        <time className="text-sm">{item.date}</time>
+                                    </div>
+                                </li>
+                            )
+                        })}
+                    </ul>
                 </>
                 :
                 <div className="flex flex-col gap-1 items-center justify-center h-full">
