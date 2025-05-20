0dec06230ac33304ec82f0763d62cef5d99acae2
mthquevedo <matheusq10@gmail.com>
2025-05-20 10:15:56 -0300

    Ajuste: margens removidas

--- src/components/LoadingCards/index.tsx
+++ src/components/LoadingCards/index.tsx
@@ -8,7 +8,7 @@ export function LoadingCards({ quantity }: LoadingCardsProps) {
     return (
         <>
             {cards.map((_, i) => (
-                <div key={i} className="bg-slate-200 w-12v h-[17rem] mt-6 mb-0.5 rounded-2xl shadow-md overflow-hidden">
+                <div key={i} className="bg-slate-200 w-12v h-[17.5rem] rounded-2xl shadow-md overflow-hidden">
                     <div className="animate-pulse">
                         <div className="bg-slate-400/60 flex flex-col justify-center items-center w-full h-48 relative overflow-hidden rounded-xl">
                         </div>
