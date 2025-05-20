a9c13fccc5cad2ff3364c9c03ff0c4558735ed36
mthquevedo <matheusq10@gmail.com>
2025-05-19 21:23:24 -0300

    Ajuste: páginas renomeadas e theme toogle removido

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,16 +1,14 @@
 import { useTranslation } from "react-i18next";
-import { BsList } from "react-icons/bs";
-import { HiOutlineHome } from "react-icons/hi";
-import { HiOutlineSparkles } from "react-icons/hi2";
+import { HiHome, HiOutlineSparkles } from "react-icons/hi2";
+import { IoMdListBox } from "react-icons/io";
 import { MdCatchingPokemon } from "react-icons/md";
 import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
-import { LoadingScreen } from "../components/LoadingScreen";
-import { MenuItem } from "../components/Menu/MenuItem";
-import { ThemeToggle } from "../components/Menu/ThemeToggle";
-import { RootState } from "../store";
 import { FooterPage } from "../components/FooterPage";
 import { HeaderPage } from "../components/HeaderPage";
+import { LoadingScreen } from "../components/LoadingScreen";
+import { MenuItem } from "../components/Menu/MenuItem";
+import { RootState } from "../store";
 
 export const MainLayout = () => {
     const isLoading = useSelector((state: RootState) => state.allPokemonList.loading);
@@ -22,23 +20,27 @@ export const MainLayout = () => {
                 <LoadingScreen />
                 :
                 <>
-                    <aside className="flex flex-col justify-between items-center bg-menuback w-4v h-100v py-8 border-r border-neutral-500">
+                    <aside className="flex flex-col justify-between items-center bg-menuback w-5v h-100v py-6 border-r border-neutral-500 overflow-hidden overflow-y-hidden">
                         <div className="flex flex-col items-center">
-                            <img className="h-auto w-14 rounded-lg mb-2" src="src/assets/logo.jpg" alt="Logotipo" />
+                            <img className="h-auto w-14 rounded-lg" src="src/assets/logo.jpg" alt="Logotipo" />
 
-                            <nav className="flex flex-col justify-center items-center px-5 mt-12 gap-10 text-gray-50">
-                                <MenuItem icon={HiOutlineHome} altText={"Ícone do Início"} text={t("sidebar.home")} to={"/"} />
-                                <MenuItem icon={MdCatchingPokemon} altText={"Ícone da Pokédex"} text={"Pokédex"} to={"pokedex"} />
-                                <MenuItem icon={BsList} altText={"Ícone da Lista de desejos"} text={"Lista de desejos"} to={"listadedesejos"} />
-                                <MenuItem icon={HiOutlineSparkles} altText={"Ícone dos Meus shinys"} text={"Meus shinys"} to={"shinys"} />
+                            <nav className="flex flex-col justify-center items-center px-5 mt-[1.8rem] gap-6 text-gray-50">
+                                <MenuItem icon={HiHome} altText={"Ícone do Início"} text={t("sidebar.home")} to={"/"} />
+                                <MenuItem icon={MdCatchingPokemon} altText={"Ícone da Pokédex"} text={"Dex"} to={"pokedex"} />
+                                <MenuItem icon={IoMdListBox} altText={"Ícone da Lista de desejos"} text={"Desejos"} to={"listadedesejos"} />
+                                <MenuItem icon={HiOutlineSparkles} altText={"Ícone dos Meus shinys"} text={"Shinys"} to={"shinys"} />
                             </nav>
                         </div>
 
-                        <ThemeToggle />
+                        <div className="flex flex-col justify-center gap-4">
+                            <hr className="border-stone-400 mx-1" />
+
+                            <p className="bg-neutral-700 p-1 font-medium text-xs text-stone-300 border border-neutral-600 rounded-md">Versão 1.0</p>
+                        </div>
                     </aside>
 
-                    <main>
-                        <section className="flex flex-col justify-between bg-gradient-to-tl from-backcolor to-white w-95v h-100v pt-11 pb-9 px-10 ">
+                    <main className="overflow-hidden">
+                        <section className="flex flex-col justify-between bg-gradient-to-tl from-backcolor to-white w-95v h-100v pt-11 pb-6 px-10">
                             <HeaderPage />
 
                             <Outlet />
