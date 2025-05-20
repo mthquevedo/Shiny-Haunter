a4e08930a5b19264f2c2cd0824bddd671ef3505e
mthquevedo <matheusq10@gmail.com>
2025-05-20 11:10:31 -0300

    Criação: uma interface global foi criada e substituiu a anterior

--- src/constants/pokemon.constants.ts
+++ src/constants/pokemon.constants.ts
@@ -1,16 +1,12 @@
-import { Pokemon, PokemonType } from "pokenode-ts";
+import { PokemonType } from "pokenode-ts";
 
 export const LIMIT_CARDS = 14;
 
-export interface PokeCardProps {
-    id?: number;
-    name?: string;
-    thumbnailDefault?: string;
-    thumbnailShiny?: string;
-    types?: PokemonType[];
-}
-
-export interface PokemonWithImages extends Pokemon {
+export interface PokeProps {
+    id: number;
+    key: number;
+    name: string;
     thumbnailDefault: string;
     thumbnailShiny: string;
+    types: PokemonType[];
 }
