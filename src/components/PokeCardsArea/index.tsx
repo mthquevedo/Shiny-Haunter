88ee15376494b3d9f879dbafd0302be640dfdfa1
mthquevedo <matheusq10@gmail.com>
2025-05-20 10:17:03 -0300

    Adição: nova validação para reset de estado e adição de margem no layout

--- src/components/PokeCardsArea/index.tsx
+++ src/components/PokeCardsArea/index.tsx
@@ -1,13 +1,15 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { LIMIT_CARDS } from "../../constants/pokemon.constants";
 import { usePokedex } from "../../hooks/usePokedex";
 import { RootState } from "../../store";
+import { setSearchList } from "../../store/reducers/searchPokedex";
 import { idValidation, nameValidation, typeValidation } from "../../utils/cardValidation";
 import { LoadingCards } from "../LoadingCards";
 import { PokeCard } from "../PokeCard";
 
 export function PokeCardsArea() {
+    const dispatch = useDispatch();
     const { loading, list } = useSelector((state: RootState) => state.pokedexList);
     const searchList = useSelector((state: RootState) => state.searchPokedex.searchList);
     const cardsQuantity = searchList.length === 0 ? LIMIT_CARDS : searchList.length;
@@ -16,11 +18,12 @@ export function PokeCardsArea() {
     useEffect(() => {
         if (list.length !== LIMIT_CARDS) {
             getPokemonPage();
+            dispatch(setSearchList([]));
         }
     }, []);
 
     return (
-        <div className="w-full h-full flex items-start justify-start flex-wrap gap-6">
+        <div className="w-full h-full flex items-start justify-start mt-4 flex-wrap gap-5">
             {loading ?
                 <LoadingCards quantity={cardsQuantity} />
                 :
