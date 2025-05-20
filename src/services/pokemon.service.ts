1ac329c2bc2c295f4e5aed47fb9f6e36af0ea59e
mthquevedo <matheusq10@gmail.com>
2025-05-20 11:08:40 -0300

    Refatoração:  remoção do getImage e refatoração para funções retornarem objetos serializados

--- src/services/pokemon.service.ts
+++ src/services/pokemon.service.ts
@@ -1,30 +1,24 @@
-import { PokemonWithImages } from "../constants/pokemon.constants";
 import { upperFirstLetter } from "../utils/pokemon.utils";
+import { serializePokemon } from "../utils/pokemonSerializer";
 import { pokemonClient } from "./pokemon.client";
-
-function getPokemonImageUrl(variant: "default" | "shiny", id: number) {
-    const imgUrl = `https://res.cloudinary.com/dgk1m8bko/image/upload/f_auto,q_auto,w_176/pokemon/${variant}/${id}.png`;
-
-    return imgUrl ?? "";
-}
 
 class PokemonServices {
     async getLimitedPokemons(offset: number, limit: number) {
         const { results, ...pagination } = await pokemonClient.listPokemons(offset, limit);
 
-        const pokemons: PokemonWithImages[] = await Promise.all(
+        const pokemons = await Promise.all(
             results.map(async ({ name }) => {
                 const data = await pokemonClient.getPokemonByName(name);
-
-                return {
-                    ...data,
-                    thumbnailDefault: getPokemonImageUrl("default", data.id),
-                    thumbnailShiny: getPokemonImageUrl("shiny", data.id),
-                };
+                return serializePokemon(data);
             })
         );
 
         return { pokemons, ...pagination };
+    }
+
+    async getSinglePokemon(name: string) {
+        const data = await pokemonClient.getPokemonByName(name);
+        return serializePokemon(data);
     }
 
     async getAllPokemons(offset = 0, limit = 1302) {
@@ -40,16 +34,6 @@ class PokemonServices {
         }))
 
         return allPokemonList;
-    }
-
-    async getSinglePokemon(name: string) {
-        const data = await pokemonClient.getPokemonByName(name);
-
-        return {
-            ...data,
-            thumbnailDefault: getPokemonImageUrl("default", data.id),
-            thumbnailShiny: getPokemonImageUrl("shiny", data.id),
-        };
     }
 }
 
