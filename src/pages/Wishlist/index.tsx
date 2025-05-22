b38295ff47938194d0986bb43fd9bf3cda9cd1da
mthquevedo <matheusq10@gmail.com>
2025-05-22 19:29:49 -0300

    Ajuste: componente atualizado com a nova estrutura do IntroArea

--- src/pages/Wishlist/index.tsx
+++ src/pages/Wishlist/index.tsx
@@ -1,16 +1,26 @@
-import { IntroArea } from "../../components/introArea";
-import { WishCardsArea } from "../../components/WishCardsArea";
+import { useSelector } from "react-redux";
+import { GenericCardsArea } from "../../components/GenericCardsArea";
+import { IntroArea } from "../../components/IntroArea";
+import { RootState } from "../../store";
 
 export function Wishlist() {
+    const { list, view } = useSelector((state: RootState) => state.wishlist);
+    
     return (
         <>
-            <IntroArea
-                title="Lista de desejos"
-                description="Adicione os pokémon às listas de desejos ou capturados, caso prefira, busque pelo nome específico na barra de pesquisa."
-            />
+            <IntroArea.Root>
+                <IntroArea.Texts
+                    title="Lista de desejos"
+                    description="Adicione os pokémon às listas de desejos ou capturados, caso prefira, busque pelo nome específico na barra de pesquisa."
+                />
+                <IntroArea.ViewButton />
+            </IntroArea.Root>
 
-            <section className="flex flex-col justify-around gap-5 w-full h-75v">
-                <WishCardsArea />
+            <section className="flex flex-col gap-5 w-full h-75v">
+                <GenericCardsArea
+                    list={list}
+                    view={view}
+                />
             </section>
         </>
     )
