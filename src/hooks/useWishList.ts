b7e007a8f7feab91fad257133d24a852c91927d2
mthquevedo <matheusq10@gmail.com>
2025-05-23 13:12:20 -0300

    Adição: novo método para resetar a notificação e ajuste de identação

--- src/hooks/useWishList.ts
+++ src/hooks/useWishList.ts
@@ -1,19 +1,25 @@
 import { useDispatch } from "react-redux";
 import { genericSubListItem } from "../constants/pokemon.constants";
-import { toggleWishlist, deleteWishItem } from "../store/reducers/wishlist";
+import { deleteWishItem, resetNotification, toggleWishlist } from "../store/reducers/wishlist";
 
 export function useWishList() {
     const dispatch = useDispatch();
 
     const handleWishItem = (pokemon: genericSubListItem) => {
         dispatch(toggleWishlist(pokemon));
-    }
+    };
+
     const deleteItem = (name: string) => {
         dispatch(deleteWishItem(name));
-    }
+    };
+
+    const resetNotificationState = () => {
+        dispatch(resetNotification())
+    };
 
     return {
         handleWishItem,
         deleteItem,
+        resetNotificationState,
     };
 }
