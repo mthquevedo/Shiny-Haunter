e60c05e8a8426d2479f7917d898c2c54cc44b14f
mthquevedo <matheusq10@gmail.com>
2025-05-23 11:41:53 -0300

    Adição: botão de remover item adicionado e atributos de flex box ao container

--- src/components/GenericCard/GenericCardGrid.tsx
+++ src/components/GenericCard/GenericCardGrid.tsx
@@ -1,19 +1,21 @@
 import { genericPokemon } from "../../constants/pokemon.constants";
 import { TypeShelf } from "../PokeCard/typeShelf";
+import { DeleteButton } from "./deleteButton";
+
 
 export function GenericCardGrid({ pokemon }: genericPokemon) {
     return (
         <article
             key={pokemon.id}
-            className="mr-2 flex flex-col items-center bg-neutral-300 w-8v rounded-lg p-2"
+            className="flex flex-col items-center bg-neutral-300 w-8v rounded-lg p-2"
         >
             <header className="flex w-full items-center justify-between pb-2">
                 <TypeShelf types={pokemon.type} />
 
-                <div className="">
-                    <button>b1</button>
-
+                <div className="flex gap-2">
                     <button>b2</button>
+
+                    <DeleteButton name={pokemon.name} />
                 </div>
             </header>
 
@@ -21,7 +23,7 @@ export function GenericCardGrid({ pokemon }: genericPokemon) {
                 src={pokemon.image}
                 alt={`Imagem do pokémon ${pokemon.name}`}
                 loading="lazy"
-                className=""
+                className="w-36"
             />
 
             <h3 className="">{pokemon.name}</h3>
