f89158cd08f9ba2611d65dbc44ce21abffc879de
mthquevedo <matheusq10@gmail.com>
2025-05-26 19:50:40 -0300

    Remoção: view state removido

--- src/pages/Wishlist/index.tsx
+++ src/pages/Wishlist/index.tsx
@@ -6,9 +6,8 @@ import { useWishList } from "../../hooks/useWishList";
 import { RootState } from "../../store";
 
 export function Wishlist() {
-    const { list, view } = useSelector((state: RootState) => state.wishlist);
     const { resetNotificationState } = useWishList();
-    const { hasWishNotification } = useSelector((state: RootState) => state.wishlist);
+    const { list, hasWishNotification } = useSelector((state: RootState) => state.wishlist);
 
     useEffect(() => {
         if (hasWishNotification) {
@@ -26,10 +25,9 @@ export function Wishlist() {
                 <IntroArea.ViewButton />
             </IntroArea.Root>
 
-            <section className="flex flex-col gap-5 w-full h-75v">
+            <section className="flex flex-col w-full h-75v">
                 <GenericCardsArea
                     list={list}
-                    view={view}
                 />
             </section>
         </>
