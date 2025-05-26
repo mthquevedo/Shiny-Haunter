7205bebb38f5485e7822501113e83f519882746c
mthquevedo <matheusq10@gmail.com>
2025-05-26 19:52:47 -0300

    Adição: componente de área de scroll incluso

--- src/components/GenericCardsArea/index.tsx
+++ src/components/GenericCardsArea/index.tsx
@@ -1,16 +1,32 @@
+import { ScrollArea } from "radix-ui";
+import { useSelector } from "react-redux";
 import { genericSubListProps } from "../../constants/pokemon.constants";
+import { RootState } from "../../store";
 import { GenericCard } from "../GenericCard";
 
-export function GenericCardsArea({ list, view }: genericSubListProps) {
+export function GenericCardsArea({ list }: genericSubListProps) {
+    const { view } = useSelector((state: RootState) => state.userPreferences);
+
     return (
-        <div className="w-full h-fit max-h-full flex items-start justify-start mt-4 flex-wrap gap-x-3 gap-y-2">
-            {list.map(pokemon =>
-                <GenericCard
-                    key={pokemon.id}
-                    pokemon={pokemon}
-                    view={view}
-                />
-            )}
-        </div >
+        <ScrollArea.Root className="w-full h-fit max-h-full overflow-hidden">
+            <ScrollArea.Viewport className="size-full">
+                <div className="flex items-start justify-start mt-4 flex-wrap gap-x-[1.2rem] gap-y-4">
+                    {list.map(pokemon =>
+                        <GenericCard
+                            key={pokemon.id}
+                            pokemon={pokemon}
+                            view={view}
+                        />
+                    )}
+                </div >
+            </ScrollArea.Viewport>
+            <ScrollArea.Scrollbar
+                className="flex touch-none select-none bg-neutral-300 p-0.5 transition-colors duration-[160ms] rounded-full ease-out data-[orientation=horizontal]:h-2.5 data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:flex-col hover:bg-neutral-400"
+                orientation="vertical"
+            >
+                <ScrollArea.Thumb className="relative flex-1 rounded-full bg-neutral-500 before:absolute before:left-1/2 before:top-1/2 before:size-full before:min-h-10 before:min-w-10 before:-translate-x-1/2 before:-translate-y-1/2" />
+            </ScrollArea.Scrollbar>
+            <ScrollArea.Corner className="bg-black" />
+        </ScrollArea.Root>
     );
 }
