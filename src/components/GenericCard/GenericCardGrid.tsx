0276aa019841e076232eb2e2cc1d3a5b350b9d2d
mthquevedo <matheusq10@gmail.com>
2025-05-26 19:53:35 -0300

    Adição: estilos adicionados

--- src/components/GenericCard/GenericCardGrid.tsx
+++ src/components/GenericCard/GenericCardGrid.tsx
@@ -1,32 +1,35 @@
 import { genericPokemon } from "../../constants/pokemon.constants";
+import { cn } from "../../lib/cn";
+import { bannerTypeColor } from "../../utils/pokemon.utils";
+import { CatchButton } from "../PokeCard/catchButton";
 import { TypeShelf } from "../PokeCard/typeShelf";
 import { DeleteButton } from "./deleteButton";
-
 
 export function GenericCardGrid({ pokemon }: genericPokemon) {
     return (
         <article
             key={pokemon.id}
-            className="flex flex-col items-center bg-neutral-300 w-8v rounded-lg p-2"
+            className={cn("flex flex-col items-center border border-neutral-400 w-[9.625rem] h-22v rounded-lg py-2 px-2 overflow-hidden transition hover:border-neutral-500 hover:shadow-lg", bannerTypeColor[pokemon.type[0].type.name])}
         >
-            <header className="flex w-full items-center justify-between pb-2">
-                <TypeShelf types={pokemon.type} />
+            <header className="flex w-full items-center justify-between">
+                <CatchButton />
 
-                <div className="flex gap-2">
-                    <button>b2</button>
-
-                    <DeleteButton name={pokemon.name} />
-                </div>
+                <DeleteButton name={pokemon.name} />
             </header>
 
             <img
                 src={pokemon.image}
                 alt={`Imagem do pokémon ${pokemon.name}`}
                 loading="lazy"
-                className="w-36"
+                className="w-24"
             />
 
-            <h3 className="">{pokemon.name}</h3>
+            <footer className="flex flex-col gap-0 w-full items-center justify-center">
+                <span className="scale-[0.8]">
+                    <TypeShelf types={pokemon.type} />
+                </span>
+                <h3 className="font-medium m-0 p-0 text-neutral-700 mt-0.5">{pokemon.name}</h3>
+            </footer>
         </article>
     )
 }
