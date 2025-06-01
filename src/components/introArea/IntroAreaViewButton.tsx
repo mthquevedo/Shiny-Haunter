77449094fb944de1df2dcb9a06480a98fc7c1ee6
mthquevedo <matheusq10@gmail.com>
2025-06-01 14:25:41 -0300

    Ajuste: remoção de espaço no classname

--- src/components/introArea/IntroAreaViewButton.tsx
+++ src/components/introArea/IntroAreaViewButton.tsx
@@ -10,7 +10,7 @@ export function IntroAreaViewButton() {
             <p className="text-xs font-medium mb-1">Visualização:</p>
             <div className="flex items-center justify-end gap-2">
                 <button
-                    className="flex items-center justify-center w-9 h-9  rounded-lg transition active:scale-95 duration-100 hover:shadow-md bg-primary hover:bg-indigo-700"
+                    className="flex items-center justify-center w-9 h-9 rounded-lg transition active:scale-95 duration-100 hover:shadow-md bg-primary hover:bg-indigo-700"
                     onClick={() => changeView("grid")}
                 >
                     <BsFillGridFill className="text-white text-lg" />
