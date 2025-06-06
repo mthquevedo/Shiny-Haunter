c514931d36a7ac7f58a21e2a7e6b79340f8b2456
mthquevedo <matheusq10@gmail.com>
2025-06-06 19:52:15 -0300

    Ajuste: componente atualizado para comportar diferentes telas

--- src/components/FooterPage/index.tsx
+++ src/components/FooterPage/index.tsx
@@ -4,11 +4,22 @@ export function FooterPage() {
     const isHomePage = useMatch("/");
 
     return (
-        <div className="text-xs w-full flex items-center justify-between text-darkgray">
-            {isHomePage && (
-                <p className="font-medium mt-3">*Projeto sem fins lucratrivos.</p>
-            )}
-            <p className="flex-1 text-right font-bold">Criador por <a href="https://github.com/mthquevedo" target="_blank" className="text-primary hover:text-indigo-800">@Matheus Quevedo</a></p>
-        </div>
+        <>
+            {/* Versão Mobile */}
+            {isHomePage &&
+                <footer className="flex md:hidden flex-col md:flex-row items-start md:items-center justify-start text-[0.68rem] text-darkgray w-full h-[2%] mb-4">
+                    <p className="font-medium">Projeto sem fins lucratrivos.</p>
+                    <p className="flex-1 md:text-right font-bold">Criador por <a href="https://github.com/mthquevedo" target="_blank" className="text-primary hover:text-indigo-800">@Matheus Quevedo</a></p>
+                </footer>
+            }
+
+            {/* Versão Web */}
+            <footer className="hidden md:flex flex-col md:flex-row items-start md:items-center justify-start text-xs text-darkgray w-full h-[2%] mt-6 lg:mt-4">
+                {isHomePage && (
+                    <p className="font-medium">*Projeto sem fins lucratrivos.</p>
+                )}
+                <p className="flex-1 md:text-right font-bold">Criador por <a href="https://github.com/mthquevedo" target="_blank" className="text-primary hover:text-indigo-800">@Matheus Quevedo</a></p>
+            </footer>
+        </>
     );
 }
