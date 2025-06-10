ceac78ae4a30fea85f6bac397ff528808852ded5
mthquevedo <matheusq10@gmail.com>
2025-06-10 19:46:50 -0300

    Ajuste: condicional de exibição adicionada

--- src/components/FooterPage/index.tsx
+++ src/components/FooterPage/index.tsx
@@ -1,7 +1,21 @@
+import { useMatch } from "react-router-dom";
+
 export function FooterPage() {
+    const isHomePage = useMatch("/");
+
     return (
-        <footer className="flex items-center justify-end w-full md:mt-6 lg:mt-4 2xl:mt-2">
-            <p className="text-right font-bold text-darkgray text-[0.5rem] xsm:text-[0.6rem] md:text-xs lg:text-[0.625rem]">Criador por <a href="https://github.com/mthquevedo" target="_blank" className="text-primary hover:text-indigo-800">@Matheus Quevedo</a></p>
-        </footer>
+        <>
+            {/* Versão mobile */}
+            {isHomePage &&
+                <footer className="flex md:hidden items-center justify-end w-full md:mt-6 lg:mt-4 2xl:mt-2">
+                    <p className="text-right font-bold text-darkgray text-[0.5rem] xsm:text-[0.6rem] md:text-xs lg:text-[0.625rem]">Criador por <a href="https://github.com/mthquevedo" target="_blank" className="text-primary hover:text-indigo-800">@Matheus Quevedo</a></p>
+                </footer>
+            }
+
+            {/* Versão PC */}
+            <footer className="hidden md:flex items-center justify-end w-full md:mt-6 lg:mt-0 2xl:mt-2">
+                <p className="text-right font-bold text-darkgray text-[0.5rem] xsm:text-[0.6rem] md:text-xs lg:text-[0.625rem]">Criador por <a href="https://github.com/mthquevedo" target="_blank" className="text-primary hover:text-indigo-800">@Matheus Quevedo</a></p>
+            </footer>
+        </>
     );
 }
