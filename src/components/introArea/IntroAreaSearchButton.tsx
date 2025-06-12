50b35568f9b53bf00cae1af591e0589b93bcd1d2
mthquevedo <matheusq10@gmail.com>
2025-06-11 21:25:28 -0300

    Ajuste: dimensões ajustadas para telas "lg"

--- src/components/introArea/IntroAreaSearchButton.tsx
+++ src/components/introArea/IntroAreaSearchButton.tsx
@@ -6,7 +6,7 @@ export function IntroAreaSearchButton() {
 
     return (
         <button
-            className="flex items-center justify-center w-10 h-10 bg-primary hover:bg-indigo-800 shadow-sm rounded-lg"
+            className="flex items-center justify-center w-11 lg:w-10 h-full bg-primary hover:bg-indigo-800 shadow-sm rounded-lg"
             onClick={() => getSearchedPokemon()}
         >
             <IoMdSearch className="text-white text-2xl" />
