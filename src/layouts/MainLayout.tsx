1a96254948f4451260f88d802b1553560c1a8d96
mthquevedo <matheusq10@gmail.com>
2025-06-16 21:55:07 -0300

    Ajuste: mudança de formatação do layout de flexbox

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -17,10 +17,12 @@ export const MainLayout = () => {
                 <>
                     <Menu />
 
-                    <main className="flex flex-col justify-between bg-gradient-to-tl bg-backcolor w-full md:w-90v md:max-w-89v lg:w-93v xl:w-94v 2xl:w-96v h-92v xsm:h-93v md:h-100v px-7 lg:px-10 pt-4 pb-2 xsm:pb-3 md:pt-9 xl:pt-6 md:pb-6 overflow-hidden">
-                        <HeaderPage />
+                    <main className="flex flex-col items-center justify-between bg-backcolor w-full md:w-90v md:max-w-89v lg:w-93v xl:w-94v 2xl:w-96v h-92v xsm:h-93v md:h-100v px-7 lg:px-10 pt-4 pb-2 xsm:pb-3 md:pt-9 xl:pt-6 md:pb-6 overflow-hidden">
+                        <div className="flex flex-col justify-start h-[95%] w-full">
+                            <HeaderPage />
 
-                        <Outlet />
+                            <Outlet />
+                        </div>
 
                         <FooterPage />
                     </main>
