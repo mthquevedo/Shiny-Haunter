620c7d35d3f165064968b52f379296d93d48c116
mthquevedo <matheusq10@gmail.com>
2025-06-19 20:41:09 -0300

    Ajuste: padrões de responsividade aplicados

--- src/components/GenericCard/GenericCardList.tsx
+++ src/components/GenericCard/GenericCardList.tsx
@@ -13,10 +13,10 @@ export function GenericCardList({ pokemon, hasCatchButton }: GenericCardListProp
     return (
         <article
             key={pokemon.id}
-            className="flex items-center bg-white w-[24%] h-32 rounded-lg overflow-hidden shadow-md"
+            className="flex items-center bg-white w-full md:w-[48%] lg:w-[31.8%] xl:w-[32.1%] 2xl:w-[24%] h-28 xsx:h-32 lg:h-28 2xl:h-32 rounded-lg overflow-hidden shadow-md"
         >
             <section
-                className={cn("flex items-center justify-center w-[35%] h-full p-3 rounded-lg", bannerTypeColor[pokemon.types[0].type.name])}
+                className={cn("flex items-center justify-center w-[35%] h-full p-3 md:p-1 xl:p-2 2xl:p-3", bannerTypeColor[pokemon.types[0].type.name])}
             >
                 <img
                     src={pokemon.image}
@@ -29,18 +29,16 @@ export function GenericCardList({ pokemon, hasCatchButton }: GenericCardListProp
             <section className="flex flex-col items-start justify-between w-[65%] h-full p-3">
                 <div className="flex items-center justify-between w-full h-fit">
                     <p className="font-semibold text-neutral-500">#{pokemon.id}</p>
-                    <span className="-mr-1.5">
-                        <DeleteButton name={pokemon.name} />
-                    </span>
+                    <DeleteButton name={pokemon.name} />
                 </div>
 
                 <p className="text-xl font-semibold text-neutral-700">{pokemon.name}</p>
 
                 <div className="flex items-center justify-between w-full h-fit">
-                    <span className="scale-[0.9] -ml-1">
+                    <span className="-ml-1">
                         <TypeShelf types={pokemon.types} />
                     </span>
-                    <span className="w-[60%] -mb-1.5">
+                    <span className="w-[60%] lg:w-[65%] -mb-1">
                         {hasCatchButton && <GenericCatchButton pokemon={pokemon} />}
                     </span>
                 </div>
