35850d0ac07873a2200eb8ab2d8446afa32c6e2d
mthquevedo <matheusq10@gmail.com>
2025-06-19 14:02:02 -0300

    Ajuste: layout reformulado + nova lógica de ativação do botão

--- src/components/introArea/IntroAreaViewButton.tsx
+++ src/components/introArea/IntroAreaViewButton.tsx
@@ -1,25 +1,34 @@
 import { BsFillGridFill } from "react-icons/bs";
 import { FaList } from "react-icons/fa6";
+import { useSelector } from "react-redux";
 import { usePreferences } from "../../hooks/usePreferences";
+import { cn } from "../../lib/cn";
+import { RootState } from "../../store";
+
+const ACTIVE_BUTTON = "bg-primary text-white hover:text-white hover:bg-indigo-800";
 
 export function IntroAreaViewButton() {
+    const currentView = useSelector((state: RootState) => state.userPreferences.view);
+    const isGridView = currentView === "grid";
+    const isListView = currentView === "list";
     const { changeView } = usePreferences();
 
     return (
         <section>
             <p className="hidden lg:inline-block text-xs mb-1">Visualizar por:</p>
+
             <div className="flex items-center justify-end gap-2">
                 <button
-                    className="flex items-center justify-center w-9 h-9 rounded-lg transition active:scale-95 duration-100 hover:shadow-md bg-primary hover:bg-indigo-700"
+                    className={cn("flex items-center justify-center w-9 h-9 hover:shadow-md text-neutral-500 bg-neutral-300 hover:text-neutral-700 active:scale-95 transition duration-100 rounded-lg", { [ACTIVE_BUTTON]: isGridView })}
                     onClick={() => changeView("grid")}
                 >
-                    <BsFillGridFill className="text-white text-lg" />
+                    <BsFillGridFill className="text-lg" />
                 </button>
                 <button
-                    className="flex items-center justify-center w-9 h-9 rounded-lg transition active:scale-95 duration-100 hover:shadow-md bg-primary hover:bg-indigo-700"
+                    className={cn("flex items-center justify-center w-9 h-9 hover:shadow-md text-neutral-500 bg-neutral-300 hover:text-neutral-700 active:scale-95 transition duration-100 rounded-lg", { [ACTIVE_BUTTON]: isListView })}
                     onClick={() => changeView("list")}
                 >
-                    <FaList className="text-white text-lg" />
+                    <FaList className="text-lg" />
                 </button>
             </div>
         </section>
