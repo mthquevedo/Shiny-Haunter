e24dcd80eb1257ea8c884a63a05573172da6a2a9
mthquevedo <matheusq10@gmail.com>
2025-06-20 12:31:06 -0300

    Ajuste: o tempo de progresso do loading foi aumentado + adição de padrões de responsividade

--- src/components/LoadingScreen/index.tsx
+++ src/components/LoadingScreen/index.tsx
@@ -8,23 +8,24 @@ export function LoadingScreen() {
     const isLoading = useSelector((state: RootState) => state.allPokemonList.loading);
 
     useEffect(() => {
-        let timer = setTimeout(() => setProgress(50), 1000);
-        timer = setTimeout(() => setProgress(100), 2500);
+        let timer = setTimeout(() => setProgress(60), 1200);
+        timer = setTimeout(() => setProgress(100), 2600);
         if (isLoading === false) {
             return () => clearTimeout(timer)
         }
     }, [])
 
     return (
-        <div className="flex flex-col gap-4 justify-center items-center bg-gradient-to-tr from-indigo-800 to-primary w-100v h-100v overflow-hidden">
-            <img className="h-auto w-72" src="src/assets/Gengar loading.gif" alt="Gif do gengar" />
+        <main className="flex items-center justify-center bg-gradient-to-br from-indigo-600 to-primary w-100v h-100v overflow-hidden">
+            <section className="flex flex-col items-center justify-center gap-4 w-[80%]">
+                <img className="h-auto w-[90%] md:w-[60%] lg:w-[30%]" src="src/assets/Gengar loading.gif" alt="Animação de carregamento da página" />
 
-            <p className="text-lg text-neutral-100 mb-2">Aguarde enquanto capturamos o Gengar que fugiu...</p>
+                <h1 className="font-medium text-center text-lg md:text-xl text-neutral-100 mb-2">Aguarde enquanto capturamos o Gengar que fugiu...</h1>
 
-            <Progress.Root className="h-6 w-96 bg-slate-900 border-2 border-menucyan overflow-hidden rounded-full" value={progress}>
-                <Progress.Indicator className="ease-[cubic-bezier(0.65, 0, 0.35, 1)] size-full bg-white transition-transform duration-[660ms]" style={{ transform: `translateX(-${100 - progress}%)` }} />
-            </Progress.Root>
-
-        </div>
+                <Progress.Root className="h-6 xsm:h-7 md:h-9 lg:h-7 w-full md:w-[90%] lg:w-[75%] xl:w-[70%] 2xl:w-[60%] bg-slate-900 border-2 border-primary overflow-hidden rounded-full" value={progress}>
+                    <Progress.Indicator className="ease-[cubic-bezier(0.65, 0, 0.35, 1)] size-full bg-neutral-50 transition-transform duration-[660ms]" style={{ transform: `translateX(-${100 - progress}%)` }} />
+                </Progress.Root>
+            </section>
+        </main>
     )
 }
