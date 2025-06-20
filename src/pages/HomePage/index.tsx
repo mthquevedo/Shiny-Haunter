8f3c1a43b70fa36cf1a336d5f0414ff2b9a1a5c3
mthquevedo <matheusq10@gmail.com>
2025-06-20 15:45:51 -0300

    Ajuste: distância de gap ajustada para telas menores que 393px

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -12,10 +12,10 @@ export function HomePage() {
     return (
         <section className="w-full h-[95%]">
             <HiddenScrollBar>
-                <section className="flex flex-col items-center justify-start gap-3 xsm:gap-6 md:gap-8 lg:gap-7 2xl:gap-10 size-full">
+                <section className="flex flex-col items-center justify-start gap-5 x93:gap-3 xsm:gap-6 md:gap-8 lg:gap-7 2xl:gap-10 size-full">
                     <HeroBanner />
 
-                    <section className="flex flex-col w-full h-[56%] 2xl:justify-between lg:flex-row gap-3 xsm:gap-6 md:gap-8 lg:gap-10">
+                    <section className="flex flex-col w-full h-[56%] 2xl:justify-between lg:flex-row gap-5 x93:gap-3 xsm:gap-6 md:gap-8 lg:gap-10">
                         <LastPokemonBoard
                             list={lastWished}
                             tittle="Últimos pokémon desejados"
